018e2aafc4e22ea4e2c26e3d9089cd359c7cba7f
Shammah <contact@protoman.net>
2014-04-29 19:00:37 +0200

    Removed dependency of escope and escodegen.

--- build.sh
+++ build.sh
@@ -14,16 +14,16 @@ npm run doc
 
 # Deployment.
 echo "Deploying all files, such that it is ready for production."
-mkdir                                                          bin/scripts
-cp lib/*                                                       bin/scripts/
-cp node_modules/underscore/underscore-min.js                   bin/scripts/underscore.js
-cp node_modules/jquery/dist/jquery.min.js                      bin/scripts/jquery.js
-cp node_modules/requirejs/require.js                           bin/scripts/require.js
-cp node_modules/sweet.js/lib/*.js                              bin/scripts
-cp node_modules/sweet.js/macros/stxcase.js                     bin/scripts/stxcase.js
-cp node_modules/escodegen/escodegen.browser.min.js             bin/scripts/escodegen.js
-cp node_modules/escope/escope.js                               bin/scripts/escope.js
-cp node_modules/escope/node_modules/estraverse/estraverse.js   bin/scripts/estraverse.js
+mkdir                                                                                bin/scripts
+cp lib/*                                                                             bin/scripts/
+cp node_modules/underscore/underscore-min.js                                         bin/scripts/underscore.js
+cp node_modules/jquery/dist/jquery.min.js                                            bin/scripts/jquery.js
+cp node_modules/requirejs/require.js                                                 bin/scripts/require.js
+cp node_modules/sweet.js/lib/*.js                                                    bin/scripts
+cp node_modules/sweet.js/macros/stxcase.js                                           bin/scripts/stxcase.js
+cp node_modules/sweet.js/node_modules/escodegen/escodegen.browser.min.js             bin/scripts/escodegen.js
+cp node_modules/sweet.js/node_modules/escope/escope.js                               bin/scripts/escope.js
+cp node_modules/sweet.js/node_modules/escope/node_modules/estraverse/estraverse.js   bin/scripts/estraverse.js
 
 # We are done!
 echo "If everything went successful, all files are now in the bin/ directory."
