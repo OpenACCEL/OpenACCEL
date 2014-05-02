91decc4c61b766bdcfc8947b7e08dc34e34ba755
Shammah <contact@protoman.net>
2014-05-02 11:35:04 +0200

    Proper documentation.

--- src/model/sweet-test.js
+++ src/model/sweet-test.js
@@ -1,8 +1,8 @@
 /**
- * @author Roy Stoof
- *
- * We have two environemnts: the browser and Node. If we are in the browser we should use requirejs and call our functions that way.
+ * We have two environments: the browser and Node. If we are in the browser we should use requirejs and call our functions that way.
  * If we are in node, we should not and instead should just export the various modules for testing purpose.
+ *
+ * @author Roy Stoof
  */
 
 // Detect whether we are in the browser and should invoke a function when all required modules are loaded.
@@ -19,55 +19,60 @@ if (inBrowser) {
     });
 
     require(["sweet", "jquery"], function(sweet) {
-        compile(sweet);
+        SweetTests.compile(sweet);
     });
 }
 
 /**
- * Main and testable compilation function.
- * 
- * @param sweet A possible reference to the sweet library.
- * @return Returns the number 6 if compilation and execution of the code with Sweet.js macro has all been succesful.
+ * @class Various sweet testing functions.
  */
-var compile = function(sweet) {
-    // Try to load sweet if undefined.
-    if (sweet === undefined) {
-        var sweet = require("sweet.js");
-    }
-
-    if (inBrowser) {
-        var compileWithSourcemap = $("body").attr("data-sourcemap") === "true";
-    }
-
-    var code = "macro add { rule { ($x) } => { $x + 1 } } (function() { var test = function() { return add(5); }; x = {}; x.test1 = test; return x; })();";
-    var compiled;
-
-    try {
-        if (compileWithSourcemap) {
-            compiled = sweet.compile(code, {
-                sourceMap: false,
-                readableNames: true
-            });
-        } else {
-            compiled = sweet.compile(code, {
-                sourceMap: false,
-                readableNames: true
-            });
+var SweetTests = {
+        /**
+         * Main and testable compilation function.
+         * 
+         * @param sweet A possible reference to the sweet library.
+         * @return Returns the number 6 if compilation and execution of the code with Sweet.js macro has all been succesful.
+         */
+        compile: function(sweet) {
+        // Try to load sweet if undefined.
+        if (sweet === undefined) {
+            var sweet = require("sweet.js");
         }
 
-        var toEval = compiled.code;
-        myEval = eval(toEval);
-        var answer = myEval.test1();
         if (inBrowser) {
-            $("#output").text(answer);
+            var compileWithSourcemap = $("body").attr("data-sourcemap") === "true";
         }
-        return answer;
-    } catch (err) {
-        
+
+        var code = "macro add { rule { ($x) } => { $x + 1 } } (function() { var test = function() { return add(5); }; x = {}; x.test1 = test; return x; })();";
+        var compiled;
+
+        try {
+            if (compileWithSourcemap) {
+                compiled = sweet.compile(code, {
+                    sourceMap: false,
+                    readableNames: true
+                });
+            } else {
+                compiled = sweet.compile(code, {
+                    sourceMap: false,
+                    readableNames: true
+                });
+            }
+
+            var toEval = compiled.code;
+            myEval = eval(toEval);
+            var answer = myEval.test1();
+            if (inBrowser) {
+                $("#output").text(answer);
+            }
+            return answer;
+        } catch (err) {
+            
+        }
     }
 }
 
 // Node.js exports.
 if (inNode) {
-    exports.compile = compile;
+    exports.compile = SweetTests.compile;
 }
