d12960680d16443e382e8de17e02ecc7f8142f22
Shammah <contact@protoman.net>
2014-05-12 13:40:28 +0200

    Updated template files.

--- src/template.js
+++ src/template.js
@@ -5,45 +5,42 @@
  * @author Roy Stoof
  */
 
-// Detect whether we are in the browser and should invoke a function when all required modules are loaded.
+/** Browser vs. Node ***********************************************/
 inBrowser = typeof window !== 'undefined';
 inNode    = !inBrowser;
-
-if (inBrowser) {
-	// The browser needs to know that underscore can be used as _.
-    requirejs.config({
-        shim: {
-            'underscore': {
-                exports: '_'
-            }
-        }
-    });
-
-    // If all requirements are loaded, we may execute a main function.
-    require(["sweet", "jquery"], function(sweet) {
-        TemplateClass.compile(sweet);
-    });
+if (inNode) { 
+	require = require('requirejs');
+	sweetModule = "sweet.js";
 } else {
-	// Possible Node dependant header code.
+	sweetModule = "sweet";
 }
 
-/**
- * @class
- * @classdesc Classes can be defined as objects. Indiciate this using the @class param.
- */
-var TemplateClass = {
+require.config({
+    shim: {
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
+/*******************************************************************/
+
+// If all requirements are loaded, we may create our 'class'.
+define([sweetModule, "jquery"], function(sweet) {
+	/**
+	 * @class
+	 * @classdesc Classes can be defined as objects. Indiciate this using the @class param.
+	 */
+	var TemplateClass = {
         /**
          * Main and testable compilation function.
          * 
          * @param sweet A possible reference to the sweet library.
          * @return Returns the number 6 if compilation and execution of the code with Sweet.js macro has all been succesful.
          */
-        compile: function(sweet) {
+        compile: function() {
 	        // Try to load sweet if undefined.
 	        if (inBrowser) {
 	            var compileWithSourcemap = $("body").attr("data-sourcemap") === "true";
-	        } else if (sweet === undefined) {
-	        	var sweet = require("sweet.js");
 	        }
 
 	        var code = "macro add { rule { ($x) } => { $x + 1 } } (function() { var test = function() { return add(5); }; x = {}; x.test1 = test; return x; })();";
@@ -71,11 +68,17 @@ var TemplateClass = {
 	            return answer;
 	        } catch (err) {
 	            return -1;
-        }
-    }
-}
+        	}
+	    }
+	}
 
-// Exports are needed in Node, such that other modules may invoke methods from this module file.
-if (inNode) {
-    exports.compile = TemplateClass.compile;
-}
+	// If we are in the browser, we want to execute the compile function at the start.
+	if (inBrowser) {
+    	TemplateClass.compile();
+	}
+
+	// Exports are needed in Node, such that other modules may invoke methods from this module file.
+	if (inNode) {
+	    return TemplateClass;
+	}
+});
--- test/template.js
+++ test/template.js
@@ -1,8 +1,24 @@
-assert 	= require("assert");
-model 	= require("../src/template.js");
+requirejs = require("requirejs");
 
-suite("sweet compilation", function() {
-	test("should equal 6", function() {
-		assert.equal(6, model.compile());
+suite("template.js", function() {
+	// Template module.
+	var template;
+	var assert;
+
+    setup(function (done) {
+        // This saves the module for use in tests. You have to use
+        // the done callback because this is asynchronous.
+        requirejs(["assert", "../../../src/template"], function(assertModule, module) {
+            console.log("Loaded 'Template' module.");
+            assert = assertModule;
+            template = module;
+            done();
+        });
+    });
+
+	suite("sweet compilation", function() {
+		test("should equal 6", function() {
+			assert.equal(6, template.compile());
+		});
 	});
 });
