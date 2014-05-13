1f48c7c99cbf710bc463c87042b5ef9b5ce694d6
Shammah <contact@protoman.net>
2014-05-13 16:22:20 +0200

    Removed documentation generation from test scripts.

--- test.bat
+++ test.bat
@@ -1,6 +1,46 @@
 @echo off
 REM Build before testing.
-call npm run build-win
+
+REM Cleaning.
+echo Cleaning any leftover files from previous (intermediate) builds.
+rmdir doc /S /Q
+rmdir bin /S /Q
+
+REM Jade.
+echo Compiling the view using jade.
+call npm run jade
+
+REM Deployment.
+echo Deploying all files, such that it is ready for production.
+
+mkdir                                                                                  bin\scripts
+mkdir                                                                                  bin\img
+
+xcopy lib                                                                              bin\scripts\ /S
+copy node_modules\underscore\underscore-min.js                                         bin\scripts\underscore.js
+copy node_modules\jquery\dist\jquery.min.js                                            bin\scripts\jquery.js
+copy node_modules\requirejs\require.js                                                 bin\scripts\require.js
+copy node_modules\sweet.js\lib\*.js                                                    bin\scripts
+copy node_modules\sweet.js\macros\stxcase.js                                           bin\scripts\stxcase.js
+copy node_modules\sweet.js\node_modules\escodegen\escodegen.browser.min.js             bin\scripts\escodegen.js
+copy node_modules\sweet.js\node_modules\escope\escope.js                               bin\scripts\escope.js
+copy node_modules\sweet.js\node_modules\escope\node_modules\estraverse\estraverse.js   bin\scripts\estraverse.js
+
+cd src
+
+for /r %%d in (*.js) do copy "%%d" "..\bin\scripts"
+for /r %%d in (*.png) do copy "%%d" "..\bin\img"
+copy accelstyle.css ..\bin\
+
+cd ..
+
+echo Creating .gitignore files.
+copy NUL doc\.gitignore
+copy NUL bin\.gitignore
+
+REM We are done!
+echo If everything went successful, all files are now in the bin/ directory.
+echo Type 'npm start' to start a local HTTP server and test the program out at 'http://localhost:8080'.
 
 REM Actual testing using mocha.
 echo "Testing..."
--- test.sh
+++ test.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
-# Build before testing.
-npm run build
+# Cleaning.
+echo "Cleaning any leftover files from previous (intermediate) builds."
+rm -rf doc/
+rm -rf bin/
+
+# Jade.
+echo "Compiling the view using jade."
+npm run jade
+
+# Deployment.
+echo "Deploying all files, such that it is ready for production."
+mkdir                                                                                bin/scripts
+cp lib/*                                                                             bin/scripts/
+cp node_modules/underscore/underscore-min.js                                         bin/scripts/underscore.js
+cp node_modules/jquery/dist/jquery.min.js                                            bin/scripts/jquery.js
+cp node_modules/requirejs/require.js                                                 bin/scripts/require.js
+cp node_modules/sweet.js/lib/*.js                                                    bin/scripts
+cp node_modules/sweet.js/macros/stxcase.js                                           bin/scripts/stxcase.js
+cp node_modules/sweet.js/node_modules/escodegen/escodegen.browser.min.js             bin/scripts/escodegen.js
+cp node_modules/sweet.js/node_modules/escope/escope.js                               bin/scripts/escope.js
+cp node_modules/sweet.js/node_modules/escope/node_modules/estraverse/estraverse.js   bin/scripts/estraverse.js
+cp -r src/* bin/scripts
+find bin/scripts -type f ! -regex ".*\.s?js" -exec rm {} \;
+
+touch doc/.gitignore
+touch bin/.gitignore
+
+# We are done!
+echo "If everything went successful, all files are now in the bin/ directory."
+echo "Type 'npm start' to start a local HTTP server and test the program out at 'http://localhost:8080'."
 
 # Actual testing using mocha.
 echo "Testing..."
