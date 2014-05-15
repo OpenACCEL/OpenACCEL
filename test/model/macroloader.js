a61cf92ba7ab22ef8715d6494dd0017dc5ece643
TheRoelR <roeljacobs93@gmail.com>
2014-05-15 15:34:35 +0200

    fixed failing test cased due to platform-dependent end-of-lines

--- test/model/macroloader.js
+++ test/model/macroloader.js
@@ -1,14 +1,16 @@
 suite("Macro Loader", function() {
     var macroLoader;
     var assert;
+    var os;
 
     setup(function (done) {
         // This saves the module for use in tests. You have to use
         // the done callback because this is asynchronous.
-        requirejs(["assert", "model/macroloader"], function(assertModule, module) {
+        requirejs(["assert", "model/macroloader", "os"], function(assertModule, module, osModule) {
             console.log("Loaded 'MacroLoader' module.");
             assert = assertModule;
             macroLoader = new module();
+            os = osModule;
             done();
         });
     });
@@ -19,7 +21,7 @@ suite("Macro Loader", function() {
         });
 
         test("should match content of macro file", function() {
-            var content = "// This macro is for testing purpose only.\nmacro add {\n    rule { ($x) } => { $x + 1 }\n}";
+            var content = "// This macro is for testing purpose only." + os.EOL + "macro add {" + os.EOL + "    rule { ($x) } => { $x + 1 }" + os.EOL + "}";
             macroLoader.load("testAdd");
             assert.equal(content, macroLoader.macros["testAdd"]);
         });
@@ -35,10 +37,10 @@ suite("Macro Loader", function() {
         test("concatenation", function() {
             macroLoader.clear();
 
-            var content = "// This macro is for testing purpose only.\nmacro add {\n    rule { ($x) } => { $x + 1 }\n}";
+            var content = "// This macro is for testing purpose only." + os.EOL + "macro add {" + os.EOL + "    rule { ($x) } => { $x + 1 }" + os.EOL + "}";
             macroLoader.load("testAdd");
 
-            var content2 = "// This macro is for testing purpose only.\nmacro add {\n    rule { ($x) } => { $x + 2 }\n}";
+            var content2 = "// This macro is for testing purpose only." + os.EOL + "macro add {" + os.EOL + "    rule { ($x) } => { $x + 2 }" + os.EOL + "}";
             macroLoader.load("testAdd2");
 
             assert.equal(content + content2, macroLoader.getMacros());
