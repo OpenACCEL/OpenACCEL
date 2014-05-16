fbd3d755f905e6a2aac7cb796d702b72d22a1248
Jaczz <jacco_snoeren@live.nl>
2014-05-16 10:04:46 +0200

    Quantity updated

--- src/model/quantity.js
+++ src/model/quantity.js
@@ -6,58 +6,96 @@
  *
  * @author Jacco Snoeren
  */
-var Quantity = {
+/* Browser vs. Node ***********************************************/
+inBrowser = typeof window !== 'undefined';
+inNode = !inBrowser;
+
+if (inNode) {
+    require = require('requirejs');
+} else {
+    require.config({
+        baseUrl: 'scripts'
+    });
+}
+/*******************************************************************/
+
+define([], /**@lends Quantity*/ function() {
+    /**
+     * @class
+     * @classdesc Abstract Pass that is part of compiling the script.
+     */
+
+    function Quantity() {
+
+        /**
+         * The name of the quantity as defined in the left hand side of a line.
+         * In case of a function f(x), the name will be f.
+         * @type {String}
+         */
+        this.name = '';
+
+        /**
+         * The definition of this quantity in a String format. This is simply the right
+         * hand side of the equation.
+         * @type {String}
+         */
+        this.definition = '';
+
+        /**
+         * An array containing all quantities on which this quantity is dependent.
+         * Example: If the script contains a line 'a = b + c,'
+         * then a, b, and c are quantities and quantity a is dependent on b and c.
+         * @type {Quantity[]}
+         */
+        this.dependencies = [];
+
+        /**
+         * The category of this quantity. The category is I if the quantity is variable(input
+         * is given by the user), II if the quantity is an output variable, III if the
+         * quantity is a constant and the category is IV if it is neither of the preceding.
+         * @type {Category}
+         */
+        this.category = null;
+
+        /**
+         * The unit of this quantity in a String format. If no unit is defined, the unit is
+         * the empty string ''. Example: If the script contains a line 'a = 4 ; kg.m/s2'
+         * then the unit of quantity a is 'kg.m/s2'.
+         * @type {String}
+         */
+        this.unit = '';
+
+        /**
+         * The parameters of this quantity. If the quantity is not a function, this will be
+         * the empty array []. Example: If the script contains a line 'f(x,y) = x + y'
+         * then the parameters of quantity f are [x,y].
+         * @type {Quantity[]}
+         */
+        this.parameters = [];
+
+        /**
+         * An optional comment explaining the quantity.
+         * @type {String}
+         */
+        this.comment = '';
+
+    }
 
     /**
-     * The name of the quantity as defined in the left hand side of a line.
-     * In case of a function f(x), the name will be f.
-     * @type {String}
+     * Constructor
+     * @type {Quantity}
      */
-    name: '',
-
-    /**
-     * The definition of this quantity in a String format. This is simply the right
-     * hand side of the equation.
-     * @type {String}
-     */
-    definition: '',
-
-    /**
-     * An array containing all quantities on which this quantity is dependent.
-     * Example: If the script contains a line 'a = b + c,'
-     * then a, b, and c are quantities and quantity a is dependent on b and c.
-     * @type {Quantity[]}
-     */
-    dependencies: [],
-
-    /**
-     * The unit of this quantity in a String format. If no unit is defined, the unit is
-     * the empty string ''. Example: If the script contains a line 'a = 4 ; kg.m/s2'
-     * then the unit of quantity a is 'kg.m/s2'.
-     * @type {String}
-     */
-    unit: '',
-
-    /**
-     * The parameters of this quantity. If the quantity is not a function, this will be
-     * the empty array []. Example: If the script contains a line 'f(x,y) = x + y'
-     * then the parameters of quantity f are [x,y].
-     * @type {Quantity[]}
-     */
-    parameters: [],
-
-    /**
-     * An optional comment explaining the quantity.
-     * @type {String}
-     */
-    comment: '',
+    Quantity.prototype = new Quantity();
 
     /**
      * Returns a String representation of the line corresponding to this quantity
      * as provided by the user in the ACCEL script.
      * @return {String} the line corresponding to this quantity
      */
-    toString: function() {
+    Quantity.protoype.toString = function() {
 
-    }
-};
+    };
+
+    // Exports are needed, such that other modules may invoke methods from this module file.
+    return DependencyPass;
+});
