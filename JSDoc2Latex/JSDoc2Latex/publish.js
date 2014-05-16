91563c730fc33b2c4243f2a4d9cde5bca6fb4d6e
TheRoelR <roeljacobs93@gmail.com>
2014-05-16 19:12:05 +0200

    Fixed special character latex and added new section format

--- JSDoc2Latex/JSDoc2Latex/publish.js
+++ JSDoc2Latex/JSDoc2Latex/publish.js
@@ -4,6 +4,9 @@
 var fs = require('jsdoc/fs');
 var outdir = env.opts.destination;
 
+var membersection = "\\addcontentsline{toc}{subsubsection}{Members}\n\\subsubsection*{Members} \n";
+var methodsection = "\\addcontentsline{toc}{subsubsection}{Methods}\n\\subsubsection*{Methods} \n";
+
 var data;
 
 function getData() {
@@ -14,13 +17,24 @@ function getData() {
     }).order("longname");
 }
 
+function replaceSpecial(s) {
+    var result = s.replace(/(\\)/g, "\\textbackslash ");
+    result = result.replace("~", "\\textasciitilde ");
+    result = result.replace("^", "\\textasciicircum ");
+    result = result.replace(/([#$%&_{}])/g, function(sym) {
+        return "\\" + sym
+    });
+    return result;
+
+}
+
 function handleMember(m) {
     var output = "";
     var hasDesc = m.description;
     var hasType = m.type && m.type.names && m.type.names.length;
 
     // Section header
-    output += "\\subsubsection{" + m.name + "} \n";
+    output += "\\subsubsection{" + replaceSpecial(m.name) + "} \n";
     // constant indicator
     if (m.kind == "constant") {
         output += "$\\langle$ Constant $\\rangle$ \n";
@@ -30,7 +44,7 @@ function handleMember(m) {
         output += "\\begin{description} \n"
 
         if (hasDesc) {
-            output += "\\item[Description:]" + m.description + "\n";
+            output += "\\item[Description:]" + replaceSpecial(m.description) + "\n";
         }
         if (hasType) {
             // type is defined
@@ -45,30 +59,30 @@ function handleMember(m) {
 function handleMethod(m) {
     var output = "";
     // subsubsection for each method
-    output += "\\subsubsection{" + m.name + "(";
+    output += "\\subsubsection{" + replaceSpecial(m.name) + "(";
     if (m.params && m.params.length) {
         output += m.params[0].name;
         for (var i = 1; i < m.params.length; i++) {
-            output += ", " + m.params[i].name;
+            output += ", " + replaceSpecial(m.params[i].name);
         }
     }
     output += ")} \n";
 
     if (m.description) {
         // Description
-        output += "\\paragraph{Description:} \\hfill \\\\ \n" + m.description + "\n";
+        output += "\\paragraph{Description:} \\hfill \\\\ \n" + replaceSpecial(m.description) + "\n";
     }
     if (m.params && m.params.length) {
         // parameters
         output += "\\paragraph{Parameters:} \\hfill \\\\ \n";
-        output += "\\begin{tabular}{|l|l|l|}\n";
+        output += "\\begin{tabular}{|l|l|p{0.6\\textwidth}|}\n";
         output += "\\hline\n";
         output += "\\textbf{Name} & \\textbf{Type} & \\textbf{Description} \\\\ \n";
-        output += "\\hline\n";
+        output += "\\hline\n\\hline\n";
         for (var i in m.params) {
-            output += "\\texttt{" + ((m.params[i].name) ? m.params[i].name : "") + "} & ";
+            output += ((m.params[i].name) ? replaceSpecial(m.params[i].name) : "") + "& ";
             output += ((m.params[i].type && m.params[i].type.names && m.params[i].type.names.length) ? m.params[i].type.names[0] : "") + " & ";
-            output += ((m.params[i].description) ? m.params[i].description : "") + "\\\\ \n";
+            output += ((m.params[i].description) ? replaceSpecial(m.params[i].description) : "") + "\\\\ \n";
             output += "\\hline\n";
         }
         output += "\\end{tabular}\n";
@@ -78,7 +92,7 @@ function handleMethod(m) {
         output += "\\paragraph{Preconditions:} \n";
         output += "\\begin{itemize}  \n";
         for (var i in m.pre)
-            output += "\\item  " + m.pre[i] + "\n";
+            output += "\\item  " + replaceSpecial(m.pre[i]) + "\n";
         output += "\\end{itemize}  \n";
     }
     if (m.post && m.post.length) {
@@ -86,19 +100,19 @@ function handleMethod(m) {
         output += "\\paragraph{Postconditions:}\n";
         output += "\\begin{itemize}  \n";
         for (var i in m.post)
-            output += "\\item  " + m.post[i] + "\n";
+            output += "\\item  " + replaceSpecial(m.post[i]) + "\n";
         output += "\\end{itemize}  \n";
     }
     if (m.exceptions && m.exceptions.length) {
         // exceptions
         output += "\\paragraph{Throws:} \\hfill \\\\ \n";
-        output += "\\begin{tabular}{|l|l|}\n";
-        output += "\\hline\n";
+        output += "\\begin{tabular}{|l|p{0.6\\textwidth}|}\n";
+        output += "\\hline\n\\hline\n";
         output += "\\textbf{Type} & \\textbf{Description} \\\\ \n";
         output += "\\hline\n";
         for (var i in m.exceptions) {
             output += ((m.exceptions[i].type && m.exceptions[i].type.names && m.exceptions[i].type.names.length) ? m.exceptions[i].type.names[0] : "") + " & ";
-            output += ((m.exceptions[i].description) ? m.exceptions[i].description : "") + "\\\\ \n";
+            output += ((m.exceptions[i].description) ? replaceSpecial(m.exceptions[i].description) : "") + "\\\\ \n";
             output += "\\hline\n";
         }
         output += "\\end{tabular}\n";
@@ -107,8 +121,8 @@ function handleMethod(m) {
     if (m.returns && m.returns.length) {
         // return variable
         output += "\\paragraph{Returns:} \\hfill \\\\ \n";
-        output += ((m.returns[0].description) ? m.returns[0].description + "\\\\ \n":"");
-        output += ((m.returns[0].type && m.returns[0].type.names && m.returns[0].type.names.length) ? "\\underline{Type:} " + m.returns[0].type.names[0] + "\n": "");
+        output += ((m.returns[0].description) ? replaceSpecial(m.returns[0].description) + "\\\\ \n" : "");
+        output += ((m.returns[0].type && m.returns[0].type.names && m.returns[0].type.names.length) ? "\\underline{Type:} " + m.returns[0].type.names[0] + "\n" : "");
 
     }
     return output;
@@ -117,47 +131,44 @@ function handleMethod(m) {
 function handleClass(c) {
     var output = "";
     // new section for a new class
-    output += "\\section{" + c.name + "} \n";
+    output += "\\subsection{" + replaceSpecial(c.name) + "} \n";
     if (c.classdesc) {
         // Description of class
-        output += "\\textbf{Description: }" + c.classdesc + "\n";
+        output += "\\textbf{Description: }" + replaceSpecial(c.classdesc) + "\n";
     }
 
     // ==========================================
     // Member variables of the class
-    output += "\\subsection{Members} \n";
 
     var members = getData().filter({
-        kind: ["member", "constant"]
+        kind: ["member", "constant"],
+        memberof: c.longname
     });
+
+    if (members.count() > 0) {
+        output += membersection;
+    }
 
     // handle each member variable
     members.each(function(m) {
-            if (!m.comment || !m.memberof || !m.memberof == c.name) {
-                return;
-            }
-            output += handleMember(m);
-        }
-
-
-    );
+        output += handleMember(m);
+    });
 
     // ==========================================
     // Methods 
-    output += "\\subsection{Methods} \n";
-
     var methods = getData().filter({
-        kind: "function"
+        kind: "function",
+        memberof: c.longname
     });
+
+    if (methods.count() > 0) {
+        output += methodsection;
+    }
 
 
     // handle each method
     methods.each(function(m) {
-        if (!m.comment || !m.memberof || !m.memberof == c.name) {
-            return;
-        }
         output += handleMethod(m);
-
     });
     return output;
 }
@@ -172,7 +183,9 @@ function handleGlobals() {
     });
 
     output += "\\section{Global} \n";
-    output += "\\subsection{Members} \n";
+    if (globalMem.count() > 0) {
+        output += membersection;
+    }
 
     // Handle each global members
     globalMem.each(function(g) {
@@ -189,7 +202,9 @@ function handleGlobals() {
         kind: "function"
     });
 
-    output += "\\subsection{Methods} \n";
+    if (globalMet.count() > 0) {
+        output += methodsection;
+    }
 
     // Handle each global method
     globalMet.each(function(g) {
@@ -203,13 +218,15 @@ function handleGlobals() {
 }
 
 exports.publish = function(taffydata) {
-    var output = "";
+    var output = "{\\ttfamily\n";
 
 
     data = taffydata;
 
     // First handle all global definitions
     output += handleGlobals();
+
+    var namespaces = getData();
 
     // Extract all classes from this data
     var classes = getData().filter({
@@ -226,10 +243,7 @@ exports.publish = function(taffydata) {
 
     });
 
-    getData().each(function(r) {
-        //console.log(r);
-
-    });
+    output += "}";
 
     fs.mkPath(outdir);
     fs.writeFileSync(outdir + "jsdoc.tex", output);
