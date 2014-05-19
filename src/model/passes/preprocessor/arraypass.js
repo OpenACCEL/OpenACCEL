61e57e625e6b7fb62f3cc055261cf33d2c6b2eb0
Jaczz <jacco_snoeren@live.nl>
2014-05-19 16:27:11 +0200

    arraypass documentation + tests

--- src/model/passes/preprocessor/arraypass.js
+++ src/model/passes/preprocessor/arraypass.js
@@ -29,7 +29,8 @@ define(['model/passes/preprocessor/compilerpass'], /**@lends ArrayPass*/ functio
 
     /**
      * Translate var1[var2] where var1 & var2 are not numbers
-     * into var1.var2. For example a[b] become a.b
+     * into var1.var2 and var1.var2 into var1[var2] where var2 is a number.
+     * For example a[b] becomes a.b and a.0 becomes a[0].
      *
      * @param  {String[]} scriptLines Array of script lines.
      * @param {Report}      report A full report containing script information.
@@ -43,32 +44,45 @@ define(['model/passes/preprocessor/compilerpass'], /**@lends ArrayPass*/ functio
         CompilerPass.prototype.parse.call(this, scriptLines, report);
         return scriptLines.map(function(line) {
             // matches var1[var2] where var2 != 0
-            line = this.prototype.dotPass(scriptLines);
-            line = this.prototype.bracketPass(scriptLines);
+            line = this.prototype.dotPass(line);
+            line = this.prototype.bracketPass(line);
             return line;
+        });
+    };
+
+    /**
+     * Replaces var1[var2] by var1.var2 if var2 is not a number.
+     * Example: a[x] becomes a.x
+     * @param  {String[]} lone Array of script lines.
+     * @pre line != null && line != undefined
+     * @return {String[]} thes cript where var1[var2] is transformed in var1.var2
+     */
+    ArrayPass.prototype.bracketPass = function(line) {
+        var rline = line.match(this.regexes.squareBrackets);
+        if (rline) {
+            line = rline.replace('[', '.').replace(']', '');
         }
-    });
+        return line;
+    };
 
-    ArrayPass.prototype.bracketPass = function(scriptLines) {
-        var rline = line.match(this.regexes.squareBrackets); 
-            if (rline) {
-                line = rline.replace('[', '.').replace(']', '');
-            }
-            return line;
-    }
-
-    ArrayPass.prototype.dotPass = function(scriptLines) {
-        var rline = line.match(this.regexes.dots); 
-            if (rline) {
-                line = rline.replace(/\.\d+/, function(s) {
-                    s = s.replace('.', '[');
-                    s += ']';
-                    return s;
-                }); 
-            }
-            
-            return line;
-    }
+    /**
+     * Replaces var1.var2 into var1[var2] where var2 is a number.
+     * Example: a[0] becomes a[0]
+     * @param  {String[]} line Array of script lines.
+     * @pre line != null && line != undefined
+     * @return {String[]} the script where var1[var2] is transformed in var1.var2
+     */
+    ArrayPass.prototype.dotPass = function(line) {
+        var rline = line.match(this.regexes.dots);
+        if (rline) {
+            line = rline.replace(/\.\d+/, function(s) {
+                s = s.replace('.', '[');
+                s += ']';
+                return s;
+            });
+        }
+        return line;
+    };
 
 
     // Exports are needed, such that other modules may invoke methods from this module file.
