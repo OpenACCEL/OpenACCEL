76349d58d1a5296b73afeccbd8cf7eace58ed41f
Shiroid <shadowbowser@hotmail.com>
2014-05-20 13:49:24 +0200

    Zip function for automapping two inputs added.

---
+++ src/model/library/zip.js
@@ -0,0 +1,38 @@
+/**
+ * Applies the given function on the given array. The function is aplied recursively,
+ * so also to nested arrays.
+ * @param  {Array}   a        array taking the role of the first input of the function
+ * @param  {Array}   b        array taking the role of the second input of the function
+ * @param  {Function} func function that should be applied
+ * @return {Array}            Resulting array.
+ */
+function zip(a, b, func) {
+    if (a instanceof Array || b instanceof Array) {
+        // Recursive step, a or b is an array
+        var result = [];
+        if (!(a instanceof Array)) {
+            // Case, a is a scalar, b is an array
+            for (var key in b) {
+                result[key] = zip(a, b[key], func);
+            }
+            return result;
+        }
+        if (!(b instanceof Array)) {
+            // Case, b is a scalar, a is an array
+            for (var key in a) {
+                result[key] = zip(a[key], b, func);
+            }
+            return result;
+        }
+        // Case, a and b are both arrays
+        for (var key in a) {
+            if (key in b) {
+                result[key] = zip(a[key], b[key], func);
+            }
+        }
+        return result;
+    } else {
+        // Base: a and b are both scalar
+        return func(a, b);
+    }
+}
