4ef7778e199824ef4683096a5cdb912364e4a049
TheRoelR <roeljacobs93@gmail.com>
2014-05-20 12:40:06 +0200

    Tested map function via complier

--- test/model/library/sin.js
+++ test/model/library/sin.js
@@ -1,15 +1,15 @@
 suite("Sin Library", function() {
-    var macroExpander;
+    var compiler;
     var macros;
     var assert;
 
     setup(function(done) {
         // This saves the module for use in tests. You have to use
         // the done callback because this is asynchronous.
-        requirejs(["assert", "model/macroexpander", "model/fileloader"], function(assertModule, module, FileLoader) {
-            console.log("Loaded 'MacroExpander & FileLoader' module.");
+        requirejs(["assert", "model/compiler", "model/fileloader"], function(assertModule, module, FileLoader) {
+            console.log("Loaded 'compiler & FileLoader' module.");
             assert = assertModule;
-            macroExpander = new module();
+            compiler = new module();
             var fileLoader = new FileLoader();
             fileLoader.load("func");
             fileLoader.load("sin", "library");
@@ -18,17 +18,31 @@ suite("Sin Library", function() {
         });
     });
 
-    suite("expansion", function() {
-        test("should expand for 'x = 5'", function() {
-            var input = "exe = {};func(y = sin(5))";
-            var output = macroExpander.expand(input, macros);
-            assert.equal(Math.sin(5), eval(output)());
+    suite("Calling functions", function() {
+        test("Sin: scalar value", function() {
+            var input = "y = sin(5)";
+            var output = compiler.compile(input).exe.y();
+            assert.equal(output, Math.sin(5));
         });
 
-        test("should expand for 'x = 5, y = x + 2, z = sin(sin(x) + sin(y))'", function() {
-            var input = "exe = {};func(x = 5)func(y = sin(exe.x()) + 2)func(z = sin(sin(exe.x()) + sin(exe.y())))";
-            var output = macroExpander.expand(input, macros);
-            assert.equal(Math.sin(Math.sin(5) + Math.sin(Math.sin(5) + 2)), eval(output)());
+        test("Sin: nested function calls", function() {
+            var input = "x = 5\ny = sin(x) + 2\nz = sin(sin(x + sin(y)))";
+            var output = compiler.compile(input).exe.z();
+            assert.equal(output, Math.sin(Math.sin(5 + Math.sin(Math.sin(5) + 2))));
+        });
+
+        test("Sin: simple function", function() {
+            var input = "x = sin([1,2,3])";
+            var output = compiler.compile(input).exe.x();
+            var expected = [Math.sin(1), Math.sin(2), Math.sin(3)];
+            assert.deepEqual(output, expected);
+        });
+
+        test("Sin: simple function", function() {
+            var input = "x = sin([1,[2,[3,4]],5])";
+            var output = compiler.compile(input).exe.x();
+            var expected = [Math.sin(1), [Math.sin(2), [Math.sin(3),Math.sin(4)]], Math.sin(5)];
+            assert.deepEqual(output, expected);
         });
     });
 });
