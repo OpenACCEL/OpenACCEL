b83475dc6b67b9fb58ad18c383b27b3b9866f7ac
FalFire <FalFire@users.noreply.github.com>
2014-05-23 10:48:27 +0200

    Added AbstractView skeleton class without method declarations

--- src/controller/AbstractView.js
+++ src/controller/AbstractView.js
@@ -1,66 +1,36 @@
-suite("ControllerAPI", function() {
-    var controller;
-    var assert;
+/**
+ *
+ * @author Edward Brinkmann
+ */
 
-    setup(function (done) {
-        // This saves the module for use in tests. You have to use
-        // the done callback because this is asynchronous.
-        requirejs(["assert", "controller/ControllerAPI"], function(assertModule, module) {
-            console.log("Loaded 'Controller' module.");
-            assert = assertModule;
-            controller = new module();
-            done();
-        });
+/** Browser vs. Node ***********************************************/
+inBrowser = typeof window !== 'undefined';
+inNode = !inBrowser;
+
+if (inNode) {
+    require = require('requirejs');
+} else {
+    require.config({
+        baseUrl: "scripts"
     });
+}
+/*******************************************************************/
 
-    suite("Controlling", function() {
+// If all requirements are loaded, we may create our 'class'.
+define(["controller/ControllerAPI", "model/quantity"], function(Controller, Quantity) {
+    /**
+     * @class AbstractView
+     * @classdesc Interface declaring the methods that the view with which the Controller will 
+     * communicate should implement.
+     */
+    function AbstractView(source) {
+    }
 
-        test("Constructor tests", function() {
-            var expected = 0;
-            var output = controller.numIterations;
-            assert.equal(output,expected);
-            var output = controller.executing;
-            assert.equal(output, false);
-        });
+    
+    AbstractView.prototype = {
 
-        test("Normal Execution tests", function() {
-            controller.execute;
-        });
+    };
 
-        test("Pause Execution tests", function() {
-            var expected = false;
-            controller.stop();
-            var output = controller.executing;
-            assert.equal(output, expected);    
-        });
-
-        test("Normal get quantities tests", function() {
-            controller.getQuantities;
-        });
-
-        test("Normal add quantity tests", function() {
-            controller.addQuantity("test = b");
-        });
-
-        test("Setting iterations to valid number", function() {
-            var iterations = 1;
-            var output = controller.numIterations;
-            assert.equal(output, 0);
-            controller.setIterations(iterations);
-            output = controller.numIterations;
-            var expected = 1;
-            assert.equal(output, expected);
-        });
-
-        test("Set value in model", function() {
-            var input = "a = b + 10 ";
-            controller.addQuantity(input);
-            var input = "b = 10";
-            controller.addQuantity(input);
-            var output = controller.getQuantityValue('a');
-            var expected = 20;
-            assert.equal(output, expected);
-        });
-
-    });
+    // Exports are needed, such that other modules may invoke methods from this module file.
+    return AbstractView;
 });
