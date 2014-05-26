66aaa6e2f666edaaa9dea80018f1116ad0554934
unknown <s119587@S119587.campus.tue.nl>
2014-05-26 12:45:04 +0200

    Integrated zip and nzip into add and sum respectively.

--- src/model/library/sum.js
+++ src/model/library/sum.js
@@ -1,7 +1,9 @@
 function sum() {
-    var _sum = 0;
-    for (var i = arguments.length - 1; i >= 0; i--) {
-        _sum += arguments[i];
-    }
-    return _sum;
+    return nzip(arguments, function() {
+        var _sum = 0;
+        for (var i = arguments.length - 1; i >= 0; i--) {
+            _sum += arguments[i];
+        }
+        return _sum;
+    });
 }
