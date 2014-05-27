d69047e88625d78dd05cf46bbc00db8342f26994
TheRoelR <roeljacobs93@gmail.com>
2014-05-27 15:43:59 +0200

    additional test case

--- test/model/passes/preprocessor/historypass.js
+++ test/model/passes/preprocessor/historypass.js
@@ -23,14 +23,23 @@ suite('historypass.js', function() {
         test('parse() s = s{1} + 1', function() {
             var actual = instance.parse(['s = s{1} + 1'], {});
             var expected = ['s = history(s, 1)'];
-            assert.deepEquals(expected, actual);
+            assert.deepEqual(expected, actual);
         });
 
         test('parse() s = t{1} + 1\n t = t{2} + 2', function() {
-            var actual = instance.parse(['s = t{1} + 1', 't = t{2} + 2']);
+            var actual = instance.parse(['s = t{1} + 1', 't = t{2} + 2'], {});
             var expected = ['s = history(t, 1) + 1', 't = history(t, 2) + 2'];
-            assert.deepEquals(expected, actual);
+            assert.deepEqual(expected, actual);
         });
+
+        test('parse() s = s{cond(c{1} > 4, 5, 6)} + 1', function() {
+            var actual = instance.parse(['s = s{cond(c{1} > 4, 5, 6)} + 1'], {});
+            var expected = ['s = history(s, cond(history(c, 1) > 4, 5, 6)} + 1)'];
+            assert.deepEqual(expected, actual);
+        });
+
+
+
 
         // test('parse() s = s{t {1}}')
     });
