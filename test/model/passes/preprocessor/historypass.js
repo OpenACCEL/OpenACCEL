81d3191e6a45e86475fd9af0e08603a6a79ed96f
Jaczz <jacco_snoeren@live.nl>
2014-05-27 15:44:38 +0200

    historypass tests

--- test/model/passes/preprocessor/historypass.js
+++ test/model/passes/preprocessor/historypass.js
@@ -23,15 +23,19 @@ suite('historypass.js', function() {
         test('parse() s = s{1} + 1', function() {
             var actual = instance.parse(['s = s{1} + 1'], {});
             var expected = ['s = history(s, 1)'];
-            assert.deepEquals(expected, actual);
+            assert.deepEqual(expected, actual);
         });
 
         test('parse() s = t{1} + 1\n t = t{2} + 2', function() {
             var actual = instance.parse(['s = t{1} + 1', 't = t{2} + 2']);
             var expected = ['s = history(t, 1) + 1', 't = history(t, 2) + 2'];
-            assert.deepEquals(expected, actual);
+            assert.deepEqual(expected, actual);
         });
 
-        // test('parse() s = s{t {1}}')
+        test('parse() t=t{1}+2 \n s=s{1+t}+2', function() {
+            var actual = instance.parse(['t = t{1} + 2', 's = s{1 + t} + 2']);
+            var expected = ['t = history(t, 1) + 1', 's = history(s, 1 + t) + 2'];
+            assert.deepEqual(expected, actual);
+        });
     });
 });
