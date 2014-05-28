a1920a76928f0acbb8e98df54aad554d048f5a02
Philippe Denissen <p.e.j.g.denissen@student.tue.nl>
2014-05-28 13:31:01 +0200

    Add an ensure new line at EOF function.

--- build/build.sh
+++ build/build.sh
@@ -89,6 +89,15 @@ deploy() {
     cp -r src/view/css bin/css/
 }
 
+# Ensure each file in 'folders' with a .js extension has a new line at EOF.
+function fixnleof() {
+    local folders=(src/ test/ utils/)
+
+    for i in ${folders[@]}; do
+        find $i -type d -exec sh -c '(cd {} && for file in *; do if [[ $file == *.js ]] && [ -n "$(tail -c 1 <"$file")" ]; then echo >>"$file"; fi; done)' ';'
+    done
+}
+
 # Read command line options.
 case "$1" in
     --build)        build ;;
