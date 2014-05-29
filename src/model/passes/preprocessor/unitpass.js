4413393d280ed0972bb8a39f6d016fff864e92ef
TheRoelR <roeljacobs93@gmail.com>
2014-05-29 15:31:43 +0200

    Redid unitpass

--- src/model/passes/preprocessor/unitpass.js
+++ src/model/passes/preprocessor/unitpass.js
@@ -2,7 +2,7 @@
  * This class inherits from Pass. It performs a pass on the script to be compiled.
  * This pass processes the units.
  *
- * @author Jacco Snoeren
+ * @author Jacco Snoeren & Roel Jacobs
  */
 
 /* Browser vs. Node ***********************************************/
@@ -24,104 +24,173 @@ define(["model/passes/preprocessor/compilerpass"], /**@lends Model.Passes.Prepro
      * @class
      * @classdesc Classes can be defined as objects. Indicate this using the @class param.
      */
-    function UnitPass() {}
+    function UnitPass() {
+        this.errorMsg = '\'error\': \'uniterror\'';
+    }
 
     UnitPass.prototype = new CompilerPass();
 
     UnitPass.prototype.parse = function(scriptLines, report) {
         CompilerPass.prototype.parse.call(this, scriptLines, report);
-        var lines = []; //The result of this function {@type String[]}
-        for (var i = 0; i < scriptLines.length; i++) {
-            var line = scriptLines[i];
-            var units = this.getUnits.call(this, line);
+
+        return scriptLines.map((function(line) {
+            var units = this.getUnits(line);
             if (units) {
-                units = this.translateUnits(units);
+                var newUnits = this.translateUnits(units);
+                return line.replace(units, newUnits);
+            } else {
+                return line;
             }
-            var result = this.getLHS(line) + // left hand side
-            ' = ' +
-                this.getRHS(line) + // translated right hand side
-            ((units) ? ' ; ' + units : ''); // units if needed
-            lines.push(result);
-        }
-        return lines;
+        }).bind(this));
     };
 
     /**
-     * Translate the units of a certain line into our format, such
-     * that it is an object in the executable code.
-     * @param  {String} units the units in a String format.
-     * @pre units != null
-     * @pre units != undefined
-     * @return {String} a String to be used in our executable code, containing the units.
+     * Translates the given units.
+     *
+     * A unit is any expression of the form numerator or numerator/denominator,
+     * where both numerator and denominator consist of zero or more factors.
+     * Factors can be any string consisting of letters only, optionally followed by a positive integer, indicating the power.
+     * Factors are separated by points (.).
+     *
+     * The output is of the form
+     * "{'unit1': power1, 'unit2': power2, .... }"
+     *
+     * The powers of units appearing in the denominator are negative.
+     *
+     * In case there is an error with one of the units, the output will be
+     * "{'error': 'uniterror'}"
+     *
+     *
+     * @param  {String} units The units to translate
+     * @return {String}       Translated units
+     * @pre units != null && units != undefined && units != ''
      */
     UnitPass.prototype.translateUnits = function(units) {
         if (!units) {
             throw new Error('PreProcessor.translateUnits.pre violated' +
                 'units is null or undefined');
         }
-        var result = '{'; //the result of this function {@type String}
-        var unitsArray = UnitPass.prototype.splitUnits(units); //the elements of the units {@type String[]}
-        var inverted = false; //true when a / has occurred. All dimensions are inversed when true {@type boolean}
-        for (var i = 0; i < unitsArray.length; i++) {
-            if (unitsArray[i].match(/[a-zA-Z]/)) {
-                result += '\'' + unitsArray[i] + '\': ';
-            } else if (unitsArray[i].match(/[0-9]/)) {
-                if (inverted) {
-                    result += '-';
-                }
-                result += unitsArray[i];
-            } else if (unitsArray[i] === '.') {
-                result += ', ';
-            } else if (unitsArray[i] === '/') {
-                inverted = true;
-                result += ', ';
-            }
+        var products = units.split("/"); // split the numerator and denominator
+        var numerator = products[0];
+        var denominator = products[1];
+
+        if (!numerator || products.length > 2) {
+            // There always has to be a numerator.
+            // Also, There can be at most one devision line, so at most two
+            // elements after split. So more than two is an error
+            return '{' + this.errorMsg + '}';
         }
-        result += '}';
-        return result;
+
+        var result = [];
+        if (numerator != '1') {
+            // There is a product in the numerator, so we need to translate it.
+            var a = this.translateUnitProduct(numerator, false);
+            if (a[0] === this.errorMsg) {
+                // there was an error in the units of the numerator
+                return '{' + this.errorMsg + '}';
+            }
+            result.push.apply(result, a); // add the results;
+        }
+
+        if (denominator) {
+            // Denominator exists
+            var a = this.translateUnitProduct(denominator, true);
+            if (a[0] === this.errorMsg) {
+                // there was an error in the units of the numerator
+                return '{' + this.errorMsg + '}';
+            }
+            result.push.apply(result, a); // add the results;
+        }
+
+        // translation is ok
+        return '{' + result.join(', ') + '}';
     };
 
     /**
-     * Splits the string units into different components, which consists of only
-     * letters, digits or symbols.
-     * @param  {String} units the units in a String format
-     * @return {String[]} all components of the units
+     * Translates a string representing a product of units to an array of units
+     * in the form "'unit': power".
+     *
+     * In case no power is given, '1', will be entered.
+     *
+     * In case the given product appears in the numerator of the original unit fraction,
+     * then the 'negate' parameter shoul be set to false.
+     *
+     * In case the given product appears in the denominator of the original unit fraction,
+     * then the 'negate' parameter shoul be set to true, and a '-' will be added
+     * to each power.
+     *
+     * @param  {String} unitproduct String representing a product of units.
+     *                              Each factor in this product must be separated
+     *                              by a '.'', e.g 'kg.m2'
+     * @param {Boolean} negate      Whether the powers of the units should be negated
+     *                              (whether the factor appears in denominator).
+     * @return {String[]}           Array containing the units in the given factor in the form:
+     *                              "'unit': power". In case there is an error in the unit, it will
+     *                              be ["'error': 'uniterror'"]
+     * @pre unitproduct != null && unitproduct != undefined && unitproduct != ''
      */
-    UnitPass.prototype.splitUnits = function(units) {
-        var result = [];
-        var regex = /(?:([a-zA-Z]+)(\d*)(?:(\.)([a-zA-Z]+)(\d*))*(?:(\/)([a-zA-Z]+)(\d*)(?:(\.)([a-zA-Z]+)(\d*))*)*)/;
-        var split = units.split(regex);
-        for (var i = 0; i < split.length; i++) {
-            if (split[i] !== '' && split[i] !== undefined) {
-                result.push(split[i]);
-            }
+    UnitPass.prototype.translateUnitProduct = function(unitproduct, negate) {
+        if (!unitproduct) {
+            throw new Error('PreProcessor.translateUnitProduct.pre violated');
         }
-        result = this.addDimensionOne(result);
-        return result;
+        var units = unitproduct.split('.');
+
+        for (var i in units) {
+            var newUnit = this.translateSingleUnit(units[i], negate);
+            // Check if there is an error in the unit
+            if (newUnit === this.errorMsg) {
+                return [this.errorMsg];
+            }
+            units[i] = newUnit;
+        }
+
+        // Units processed correctly
+        return units;
     };
 
     /**
-     * Adds the dimension one to the array of units where it is explicit.
-     * @pre unitArray != null
-     * @pre unitArray != undefined
-     * @param {String[]} unitArray the elements of the units
-     * @return {String[]} the same unit array, with implicit dimension one added
+     * Translates a single unit of the form 'unit power' to the form
+     * "'unit': power".
+     *
+     * In case no power is given, '1', will be entered.
+     *
+     * When 'negate' is set to true, '-''will be entered before the power.
+     *
+     * When there is an error in the representation of the unit the result
+     * wil be "'error': 'uniterror'"
+     *
+     * @param  {String} unit   Unit to translate
+     * @param  {Boolean} negate Whether to make the powers negative.
+     * @return {String}        Unit of the form "'unit': power". "'error': 'uniterror'" when there is an error.
+     * @pre unit != null && unit != undefined && && unit != ''
      */
-    UnitPass.prototype.addDimensionOne = function(unitArray) {
-        if (!unitArray) {
-            throw new Error('PreProcessor.translateUnits.pre violated' +
-                'units is null or undefined');
+    UnitPass.prototype.translateSingleUnit = function(unit, negate) {
+        if (!unit) {
+            throw new Error('PreProcessor.translateSingleUnit.pre violated');
         }
-        var result = [];
-        for (var i = 0; i < unitArray.length; i++) {
-            result.push(unitArray[i]);
-            //if the current character is a letter and we are either at the end or the next character is not a number, we add dimension one
-            if (unitArray[i].match(/[a-zA-Z]/) && ((i === unitArray.length - 1) || !unitArray[i + 1].match(/[0-9]/))) {
-                result.push('1');
-            }
+
+        var pattern = /([a-zA-Z]+)(\d*)/; // creates to subgroups, one for the unit name, one for the power.
+
+        var match = unit.match(pattern);
+
+        var name = match[1];
+        var power = match[2];
+        var sign = negate ? "-" : "";
+
+        // check if there are no errors in the units
+        // That is, if we combine the entries of 'match', we should get
+        // the original unit-string back
+        if (name + power !== unit) {
+            return this.errorMsg;
         }
-        return result;
-    };
+
+        // make the power 1 if not present
+        power = power || '1';
+
+        return '\'' + name + '\': ' + sign + power;
+    }
+
+
 
     // Exports are needed, such that other modules may invoke methods from this module file.
     return UnitPass;
--- test/model/passes/preprocessor/unitpass.js
+++ test/model/passes/preprocessor/unitpass.js
@@ -14,8 +14,13 @@ suite("UnitPass.js", function() {
         });
     });
 
-    suite("UnitPass", function() {
-
+    suite("UnitPass.parse", function() {
+        test("parse()", function() {
+            var input = ['x = 2 ; kg2', 'y = 3 ; m/s', 'z = 3'];
+            var expResult = ['x = 2 ; {\'kg\': 2}', 'y = 3 ; {\'m\': 1, \'s\': -1}', 'z = 3'];
+            var result = UnitPass.parse(input, {});
+            assert.deepEqual(result, expResult);
+        });
 
         test("parse() robustness", function() {
             assert.throws(
@@ -27,27 +32,55 @@ suite("UnitPass.js", function() {
                     UnitPass.parse();
                 });
         });
+    });
 
-        /** Test translateUnits().
+    suite("UnitPass.translateUnits", function() {
+        /** 
+         * Test translateUnits().
          * Unit kg squared.
          */
-        test('translateUnits() kg2', function() {
+        test('kg2', function() {
             assert.deepEqual(UnitPass.translateUnits('kg2'), '{\'kg\': 2}');
         });
 
-        /** Test translateUnits().
+        /** 
+         * Test translateUnits().
          * Unit m/s.
          */
-        test('translateUnits() m/s', function() {
+        test('m/s', function() {
             assert.deepEqual(UnitPass.translateUnits('m/s'), '{\'m\': 1, \'s\': -1}');
+        });
+
+        /** 
+         * Test translateUnits().
+         * Unit 1/s.
+         */
+        test('1/s', function() {
+            assert.deepEqual(UnitPass.translateUnits('1/s'), '{\'s\': -1}');
         });
 
         /**
          * Test translateUnits().
          * Unit kg10.m/s2.t3
          */
-        test('translateUnits() kg10.m/s2.t3', function() {
+        test('kg10.m/s2.t3', function() {
             assert.deepEqual(UnitPass.translateUnits('kg10.m/s2.t3'), '{\'kg\': 10, \'m\': 1, \'s\': -2, \'t\': -3}');
+        });
+
+        /**
+         * Test translateUnits().
+         * Error: Multiple divisions
+         */
+        test('Error: Multiple divisions', function() {
+            assert.deepEqual(UnitPass.translateUnits('kg/m/s'), '{\'error\': \'uniterror\'}');
+        });
+
+        /**
+         * Test translateUnits().
+         * Error: Error in unitname
+         */
+        test('Error: Error in unitname', function() {
+            assert.deepEqual(UnitPass.translateUnits('k2g/m'), '{\'error\': \'uniterror\'}');
         });
 
         /**
@@ -62,63 +95,203 @@ suite("UnitPass.js", function() {
                 UnitPass.translateUnits();
             });
         });
+    });
 
+    suite("UnitPass.translateUnitProduct", function() {
         /**
-         * Test splitUnits()
-         * Unit kg2.
+         * Test for UnitPass.translateUnitProduct
+         * Single unit with positive power.
          */
-        test('splitUnitTest() kg2', function() {
-            assert.deepEqual(UnitPass.splitUnits('kg2'), ['kg', 2]);
+        test('Unit with positive power.', function() {
+            var input = 'm2';
+            var expResult = ['\'m\': 2'];
+            var result = UnitPass.translateUnitProduct(input, false);
+            assert.deepEqual(result, expResult);
         });
 
         /**
-         * Test splitUnits()
-         * Unit m/s.
+         * Test for UnitPass.translateUnitProduct
+         * Two Units with positive power.
          */
-        test('splitUnitTest() m/s', function() {
-            assert.deepEqual(UnitPass.splitUnits('m/s'), ['m', 1, '/', 's', 1]);
+        test('Two Units with positive power.', function() {
+            var input = 'm2.s3';
+            var expResult = ['\'m\': 2', '\'s\': 3'];
+            var result = UnitPass.translateUnitProduct(input, false);
+            assert.deepEqual(result, expResult);
         });
 
         /**
-         * Test splitUnits()
-         * Unit n42.kg5/j3.
+         * Test for UnitPass.translateUnitProduct
+         * Two Units with negative power.
          */
-        test('splitUnitTest() n42.kg5/j3', function() {
-            assert.deepEqual(UnitPass.splitUnits('n42.kg5/j3'), ['n', 42, '.', 'kg', 5, '/', 'j', 3]);
+        test('Two Units with negative power.', function() {
+            var input = 'm2.s3';
+            var expResult = ['\'m\': -2', '\'s\': -3'];
+            var result = UnitPass.translateUnitProduct(input, true);
+            assert.deepEqual(result, expResult);
         });
 
         /**
-         * Test splitUnits()
+         * Test for UnitPass.translateUnitProduct
+         * One unit without power, one with.
+         */
+        test('One unit without power, one with.', function() {
+            var input = 'm.s3';
+            var expResult = ['\'m\': 1', '\'s\': 3'];
+            var result = UnitPass.translateUnitProduct(input, false);
+            assert.deepEqual(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateUnitProduct
+         * One unit without power, one with; negative
+         */
+        test('One unit without power, one with; negative', function() {
+            var input = 'm.s3';
+            var expResult = ['\'m\': -1', '\'s\': -3'];
+            var result = UnitPass.translateUnitProduct(input, true);
+            assert.deepEqual(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateUnitProduct
+         * Long one
+         */
+        test('Long one', function() {
+            var input = 'a.b2.c3.d4.e5';
+            var expResult = ['\'a\': 1', '\'b\': 2', '\'c\': 3', '\'d\': 4', '\'e\': 5'];
+            var result = UnitPass.translateUnitProduct(input, false);
+            assert.deepEqual(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateUnitProduct
+         * Error in a unit
+         */
+        test('Error in a unit', function() {
+            var input = 's2/k.m4';
+            var expResult = ['\'error\': \'uniterror\''];
+            var result = UnitPass.translateUnitProduct(input, false);
+            assert.deepEqual(result, expResult);
+        });
+
+
+        /**
+         * Test translateUnitProduct()
          * Robustness tests.
          */
-        test('splitUnits() robustness', function() {
+        test('translateUnitProduct() robustness', function() {
             assert.throws(function() {
-                UnitPass.splitUnits(null);
+                UnitPass.translateUnitProduct(null);
             });
             assert.throws(function() {
-                UnitPass.splitUnits();
-            });
-        });
-
-        /**
-         * Test addDimensionOne()
-         * Unit kg/s.
-         */
-        test('addDimensionOne() kg/s', function() {
-            assert.deepEqual(UnitPass.addDimensionOne(['kg', '/', 's']), ['kg', 1, '/', 's', 1]);
-        });
-
-        /**
-         * Test splitUnits()
-         * Robustness tests.
-         */
-        test('addDimensionOne() robustness', function() {
-            assert.throws(function() {
-                UnitPass.addDimensionOne(null);
-            });
-            assert.throws(function() {
-                UnitPass.addDimensionOne();
+                UnitPass.translateUnitProduct();
             });
         });
     });
+
+    suite("UnitPass.translateSingleUnit", function() {
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * Unit with positive power.
+         */
+        test('Unit with positive power.', function() {
+            var input = 'm2';
+            var expResult = '\'m\': 2'
+            var result = UnitPass.translateSingleUnit(input, false);
+            assert.equal(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * Unit with negative power.
+         */
+        test('Unit with negative power.', function() {
+            var input = 'm2';
+            var expResult = '\'m\': -2'
+            var result = UnitPass.translateSingleUnit(input, true);
+            assert.equal(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * Multi-character unit with multi-digit positive power.
+         */
+        test('Multi-character unit with multi-digit positive power.', function() {
+            var input = 'kg10';
+            var expResult = '\'kg\': 10'
+            var result = UnitPass.translateSingleUnit(input, false);
+            assert.equal(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * Multi-character unit with multi-digit negative power.
+         */
+        test('Multi-character unit with multi-digit negative power.', function() {
+            var input = 'kg10';
+            var expResult = '\'kg\': -10'
+            var result = UnitPass.translateSingleUnit(input, true);
+            assert.equal(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * Unit with no power; positive
+         */
+        test('Unit with no power; positive', function() {
+            var input = 's';
+            var expResult = '\'s\': 1'
+            var result = UnitPass.translateSingleUnit(input, false);
+            assert.equal(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * Unit with no power; negative
+         */
+        test('Unit with no power; negative', function() {
+            var input = 's';
+            var expResult = '\'s\': -1'
+            var result = UnitPass.translateSingleUnit(input, true);
+            assert.equal(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * error: number in unitname
+         */
+        test('error: number in unitname', function() {
+            var input = 's2s';
+            var expResult = '\'error\': \'uniterror\'';
+            var result = UnitPass.translateSingleUnit(input, true);
+            assert.equal(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * error: non-letter symbol
+         */
+        test('error: non-letter symbol', function() {
+            var input = 's/s';
+            var expResult = '\'error\': \'uniterror\'';
+            var result = UnitPass.translateSingleUnit(input, true);
+            assert.equal(result, expResult);
+        });
+
+        /**
+         * Test for UnitPass.translateSingleUnit
+         * Robustness
+         */
+        test('robustness', function() {
+            assert.throws(function() {
+                UnitPass.translateSingleUnit(null);
+            });
+            assert.throws(function() {
+                UnitPass.translateSingleUnit();
+            });
+        });
+    });
+
+
 });
