c1e23c7ae7d34d03e3c05509d94f6adc1eb1cc52
TheRoelR <roeljacobs93@gmail.com>
2014-06-02 16:44:30 +0200

    Implementation of string replacer

--- src/model/stringreplacer.js
+++ src/model/stringreplacer.js
@@ -54,11 +54,24 @@ define([], /**@lends Model*/ function() {
          * @constant
          * @type {String}
          */
-        this.SUBSTITUTE = '\u24E2'
+        this.SUBSTITUTE = '\u24E2';
+
+        /**
+         * Regex extracting string-definitions
+         * @type {RegExp}
+         */
+        this.stringregex = /(['"])(.*?)(\1)/g;
+
+        /**
+         * Regex for extracting the places that should be restored,
+         * and immediately assigns the index tot he first matching group.
+         * @type {RegExp}
+         */
+        this.restoreregex = /(?:\u24E2)(\d+)/g;
     }
 
     /**
-     * Replaces every string definitions in the given script lines by {SUBSTITUTE}{INDEX},
+     * Replaces every string definition in the given script lines by {SUBSTITUTE}{INDEX},
      * and stores the strings in the buffer.
      *
      * Important: Replaces the content that may already be in the buffer.
@@ -67,17 +80,49 @@ define([], /**@lends Model*/ function() {
      * @return {String[]}             Array of script lines in which the strings are replaced
      */
     StringReplacer.prototype.replaceStrings = function(scriptLines) {
+        this.buffer = [];
 
+        return scriptLines.map(this.replace);
     };
 
     /**
      * Restores the strings back into the given scriptlines.
+     * For proper functioning, the method must be called with the
+     * scriptLines that replaceString() was last called with.
      * 
      * @param  {String[]} scriptLines Array with lines of Accel script
      * @return {String[]}             Array of script lines in which the strings are restored.
      */
     StringReplacer.prototype.restoreStrings = function(scriptLines) {
+        return scriptLines.map(this.restore);
+    };
 
+    /**
+     * Replaces every string definition in a single line of script by {SUBSTITUTE}{INDEX},
+     * and stores the strings in the buffer.
+     * 
+     * @param  {String} line line of Accel script
+     * @return {String}             line in which the strings are replaced
+     */
+    StringReplacer.prototype.replace = function(line) {
+        return line.replace(this.stringregex, function(match) {
+            var index = this.buffer.length;
+            this.buffer.push(match);
+
+            return this.SUBSTITUTE + index;
+        });
+    };
+
+    /**
+     * Restores the strings back into the script line.
+     * 
+     * @param  {String} line line of Accel script
+     * @return {String}             line in which the strings are replaced
+     */
+    StringReplacer.prototype.restore = function(line) {
+        return scriptLines.replace(this.restoreregex, (function(match, index) {
+            return this.buffer[index];
+        }).bind(this));
     };
 
 
