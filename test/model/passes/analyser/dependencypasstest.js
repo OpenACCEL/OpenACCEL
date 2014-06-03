251b285853b3e749d69179107c49f4b314d35da9
FalFire <FalFire@users.noreply.github.com>
2014-06-03 11:34:06 +0200

    Fixed dependencypass test

--- test/model/passes/analyser/dependencypasstest.js
+++ test/model/passes/analyser/dependencypasstest.js
@@ -23,7 +23,7 @@ suite('dependencypass.js', function() {
         /**
          * The script that we take as example for this pass.
          */
-        var script = ['a = b + c ',
+        var script = ['a = b + c',
             'b = 3',
             'c = 5',
             'f(x) = b - x',
@@ -41,80 +41,100 @@ suite('dependencypass.js', function() {
             a: {
                 name: 'a',
                 LHS: 'a',
+                source: 'a = b + c',
                 parameters: [],
                 definition: 'b + c',
                 todo: false,
                 dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             },
             b: {
                 name: 'b',
                 LHS: 'b',
+                source: 'b = 3',
                 parameters: [],
                 definition: '3',
                 todo: false,
                 dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             },
             c: {
                 name: 'c',
                 LHS: 'c',
+                source: 'c = 5',
                 parameters: [],
                 definition: '5',
                 todo: false,
                 dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             },
             f: {
                 name: 'f',
                 LHS: 'f(x)',
+                source: 'f(x) = b - x',
                 parameters: ['x'],
                 definition: 'b - x',
                 todo: false,
                 dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             },
             g: {
                 name: 'g',
                 LHS: 'g',
+                source: 'g = [2, b, x:c]',
                 parameters: [],
                 definition: '[2, b, x:c]',
                 todo: false,
                 dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             },
             h: {
                 name: 'h',
                 LHS: 'h',
+                source: 'h = [b, x:[1, y:c, b], 3]',
                 parameters: [],
                 definition: '[b, x:[1, y:c, b], 3]',
                 todo: false,
                 dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             },
             i: {
                 name: 'i',
                 LHS: 'i',
+                source: 'i = b * b',
                 parameters: [],
-                definition: 'i = b * b',
+                definition: 'b * b',
                 todo: false,
                 dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             },
             j: {
                 name: 'j',
                 LHS: 'j',
+                source: 'j = f(a) + sin(1)',
                 parameters: [],
-                definition: 'j = f(a) + sin(1)',
+                definition: 'f(a) + sin(1)',
                 todo: false,
                 dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             },
             k: {
                 name: 'k',
+                LHS: 'k',
+                source: 'k = #(i, [1,2,3], i * i, add)',
                 parameters: [],
-                definition: 'k = #(i, [1,2,3], i * i, add)',
+                definition: '#(i, [1,2,3], i * i, add)',
                 todo: false,
+                dependencies: [],
+                isTimeDependent: false,
                 reverseDeps: []
             }
         };
@@ -128,8 +148,10 @@ suite('dependencypass.js', function() {
                 LHS: 'a',
                 parameters: [],
                 definition: 'b + c',
+                source: 'a = b + c',
                 dependencies: ['b', 'c'],
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: ['j']
             },
             b: {
@@ -137,8 +159,10 @@ suite('dependencypass.js', function() {
                 LHS: 'b',
                 parameters: [],
                 definition: '3',
+                source: 'b = 3',
                 dependencies: [],
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: ['a', 'f', 'g', 'h', 'i']
             },
             c: {
@@ -146,8 +170,10 @@ suite('dependencypass.js', function() {
                 LHS: 'c',
                 parameters: [],
                 definition: '5',
+                source: 'c = 5',
                 dependencies: [],
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: ['a','g','h']
             },
             f: {
@@ -155,8 +181,10 @@ suite('dependencypass.js', function() {
                 LHS: 'f(x)',
                 parameters: ['x'],
                 definition: 'b - x',
+                source: 'f(x) = b - x',
                 dependencies: ['b'],
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: ['j']
             },
             g: {
@@ -165,7 +193,9 @@ suite('dependencypass.js', function() {
                 parameters: [],
                 dependencies: ['b','c'],
                 definition: '[2, b, x:c]',
+                source: 'g = [2, b, x:c]',
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: []
             },
             h: {
@@ -174,7 +204,9 @@ suite('dependencypass.js', function() {
                 parameters: [],
                 dependencies: ['b','c'],
                 definition: '[b, x:[1, y:c, b], 3]',
+                source: 'h = [b, x:[1, y:c, b], 3]',
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: []
             },
             i: {
@@ -183,7 +215,9 @@ suite('dependencypass.js', function() {
                 parameters: [],
                 dependencies: ['b'],
                 definition: 'b * b',
+                source: 'i = b * b',
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: []
             },
             j: {
@@ -192,15 +226,20 @@ suite('dependencypass.js', function() {
                 parameters: [],
                 dependencies: ['f','a'],
                 definition: 'f(a) + sin(1)',
+                source: 'j = f(a) + sin(1)',
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: []
             },
             k: {
                 name: 'k',
+                LHS: 'k',
                 parameters: [],
                 dependencies: [],
-                definition: 'k = #(i, [1,2,3], i * i, add)',
+                definition: '#(i, [1,2,3], i * i, add)',
+                source: 'k = #(i, [1,2,3], i * i, add)',
                 todo: false,
+                isTimeDependent: false,
                 reverseDeps: []
             }
 
@@ -211,7 +250,9 @@ suite('dependencypass.js', function() {
          */
         test('analyze()', function() {
             var result = beginReport;
+
             script.forEach(function(line) {
+                // Parse quantity name from line
                 var equalsIndex = line.indexOf('=');
                 var lhs = line.substring(0, equalsIndex).trim();
                 var qtyName =  lhs.match(/(\w*[a-zA-Z_]\w*(?!\w*\s*:))/g)[0];
@@ -219,7 +260,8 @@ suite('dependencypass.js', function() {
 
                 quantity = instance.analyse(line, quantity, result);
             });
-            //assert.deepEqual(result, endReport);
+
+            assert.deepEqual(result, endReport);
         });
     });
 });
