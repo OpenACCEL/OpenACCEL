5ea01560610b38376e1bf9a3716b2dbdd5fe062b
TheRoelR <roeljacobs93@gmail.com>
2014-06-04 16:18:56 +0200

    added scalar support to objecttoArray

--- src/model/library/objecttoarray.js
+++ src/model/library/objecttoarray.js
@@ -1,20 +1,21 @@
 /**
  * Converts an object to array.recursively
+ *
+ * If the object is a scalar value, we just return the value
+ * 
  * @param  {Object} obj object to convert
  * @return {Array}      converted array
  */
 function __objectToArray__(obj) {
-    var array = []; // Initialize the array
-    for (var key in obj) {
-        // go through each element in the object
-        // and add them to the array at the same key
-        var elem = obj[key];
-        if (elem instanceof Object) {
-            // element is an object which
-            // also has to be converted to an array.
-            elem = __objectToArray__(elem);
+    if (obj instanceof Object) {
+        var array = []; // Initialize the array
+        for (var key in obj) {
+            // go through each element in the object
+            // and add them to the array at the same key
+            array[key] = __objectToArray__(obj[key]);
         }
-        array[key] = elem;
+        return array;
+    } else {
+        return obj;
     }
-    return array;
 }
