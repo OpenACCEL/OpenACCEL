7c3bb691335e330c41c6c80fa22573bddc89170b
TheRoelR <roeljacobs93@gmail.com>
2014-06-04 17:03:42 +0200

    vAggregate testcases

---
+++ test/model/library/vAggregate.js
@@ -0,0 +1,28 @@
+suite("vAggregate Library", function() {
+    var compiler;
+    var macros;
+    var assert;
+
+    setup(function(done) {
+        // This saves the module for use in tests. You have to use
+        // the done callback because this is asynchronous.
+        requirejs(["assert", "model/compiler", "model/fileloader", "model/script"], function(Assert, module, FileLoader) {
+            console.log("Loaded 'vAggregate' module.");
+            assert = Assert;
+            compiler = new module();
+            fileLoader = new FileLoader();
+            fileLoader.load("vAggregate", "library");
+            fileLoader.load("objecttoarray", "library");
+            done();
+        });
+    });
+
+    suite("vAggregate", function() {
+
+        test("Example from help vAggregate([1,2,3,4],[10,[20,21,22]],1)=[1,10,[20,21,22],2,3,4]", function() {
+            eval(fileLoader.getContent());
+            assert.deepEqual(vAggregate([1, 2, 3, 4], [10, [20, 21, 22]], 1), [1, 10, [20, 21, 22], 2, 3, 4]);
+        });
+
+    });
+});
