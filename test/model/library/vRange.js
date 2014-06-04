929647a595c4e496843dc58f98fc19bb262d2df6
TheRoelR <roeljacobs93@gmail.com>
2014-06-04 21:19:17 +0200

    vRange test cases

---
+++ test/model/library/vRange.js
@@ -0,0 +1,79 @@
+suite("vRange Library", function() {
+    var compiler;
+    var macros;
+    var assert;
+
+    setup(function(done) {
+        // This saves the module for use in tests. You have to use
+        // the done callback because this is asynchronous.
+        requirejs(["assert", "model/compiler", "model/fileloader", "model/script"], function(Assert, module, FileLoader) {
+            console.log("Loaded 'vRange' module.");
+            assert = Assert;
+            compiler = new module();
+            fileLoader = new FileLoader();
+            fileLoader.load("vRange", "library");
+            fileLoader.load("objecttoarray", "library");
+            done();
+        });
+    });
+
+    suite("vRange", function() {
+
+        /**
+         * Test case for vRange.
+         * Based on an example of the help documentation
+         *
+         * input:vRange(['x':1,'y':2])
+         * expected: 0
+         */
+        test("vRange(['x':1,'y':2]) = 0", function() {
+            eval(fileLoader.getContent());
+
+            var expected = [1,2];
+            var input = [];
+            input.x = 1;
+            input.y = 2;
+            var result =vRange(input);
+
+            assert.deepEqual(result, expected);
+        });
+
+        /**
+         * Test case for vRange.
+         *
+         * input:vRange([1,2,3,4])
+         * expected: 0
+         */
+        test("vRange([1,2,3,4]) = 0", function() {
+            eval(fileLoader.getContent());
+
+            var expected = [1,2,3,4];
+            var input = [1,2,3,4];
+            var result =vRange(input);
+
+            assert.deepEqual(result, expected);
+        });
+
+        /**
+         * Test case for vRange.
+         * Based on an example of the help documentation
+         *
+         * input:vRange(['x':0, 1, 'y':2, 3])
+         * expected: 0
+         */
+        test("vRange(['x':0, 1, 'y':2, 3]) = 0", function() {
+            eval(fileLoader.getContent());
+
+            var expected = [1,3,0,2];
+            var input = [];
+            input.x = 0;
+            input[0] = 1;
+            input.y = 2;
+            input[1] = 3;
+            var result =vRange(input);
+
+            assert.deepEqual(result, expected);
+        });
+
+    });
+});
