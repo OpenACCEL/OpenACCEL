e1191f6ab6da3d012f9ced148cb755d62d80db49
Philippe Denissen <p.e.j.g.denissen@student.tue.nl>
2014-06-07 21:42:53 +0200

    Add SPEA skeleton class.

---
+++ src/model/emo/spea.js
@@ -0,0 +1,37 @@
+/*
+ * File containing the SPEA class
+ *
+ */
+
+/* Browser vs. Node ***********************************************/
+inBrowser = typeof window !== 'undefined';
+inNode = !inBrowser;
+
+if (inNode) {
+    require = require('requirejs');
+} else {
+    require.config({
+        baseUrl: 'scripts'
+    });
+}
+/*******************************************************************/
+
+define([], /**@lends Model.emo*/ function() {
+
+    /**
+     * @class
+     * @classdesc Class for the Strength Pareto Evolutionary Algorithm.
+     */
+    function SPEA() {
+
+        SPEA.prototype.initialise = function() {};
+
+        SPEA.prototype.mergeOffspring = function() {};
+
+        SPEA.prototype.calculateFitness = function() {};
+
+        SPEA.prototype.environmentalSelection = function() {};
+
+        SPEA.prototype.matingSelection = function() {};
+    }
+});
