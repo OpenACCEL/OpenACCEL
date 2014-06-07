07a241063066302b2cefe7454792f1a124c1b846
Philippe Denissen <p.e.j.g.denissen@student.tue.nl>
2014-06-07 19:35:09 +0200

    Add dominates function. Function is tested, tests will be added later.

--- src/model/emo/individual.js
+++ src/model/emo/individual.js
@@ -71,5 +71,35 @@ define([], /**@lends Model.emo*/ function() {
         Individual.prototype.inParetoFront = function() {
             return inParetoFront;
         };
+
+        /**
+         * Returns whether an individual dominates another individual.
+         *
+         * An individual dominates another individual if:
+         * - for every dimension individual is better than or equal to another individual
+         * - for at least one dimension individual is better than another individual
+         *
+         * @param  {Individual} individual2 another individual
+         * @pre individual2 != null
+         * @pre individual2 != undefined
+         * @return {Boolean}    whether an individual dominates another individual.
+         */
+        Individual.prototype.dominates = function(individual2) {
+            if (!individual2) {
+                throw new Error("Individual.dominates.pre is violated: individual2 is null or undefined.");
+            }
+            if (vector1.length !== vector2.length) {
+                throw new Error("Cannot compare individuals of unequal dimensions.");
+            }
+            var vector1 = vector;
+            var vector2 = individual2.getVector();
+            var worse = false;
+            var equal = true;
+            for (var i = vector1.length - 1; i >= 0 && !worse; i--) {
+                worse = vector1[i] < vector2[i];
+                equal = (vector1[i] === vector2[i]) && equal;
+            }
+            return (!equal && !worse);
+        };
     }
 });
