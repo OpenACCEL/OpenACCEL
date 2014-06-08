05681bdd144e0d22c26cc97a3ff12fc2eb4e9486
Philippe Denissen <p.e.j.g.denissen@student.tue.nl>
2014-06-08 22:08:33 +0200

    Fix syntax errors.

--- src/model/emo/spea.js
+++ src/model/emo/spea.js
@@ -22,16 +22,17 @@ define([], /**@lends Model.emo*/ function() {
      * @class
      * @classdesc Class for the Strength Pareto Evolutionary Algorithm.
      */
-    function SPEA() {
+    function SPEA() {}
 
-        SPEA.prototype.initialise = function() {};
+    SPEA.prototype.initialise = function() {};
 
-        SPEA.prototype.mergeOffspring = function() {};
+    SPEA.prototype.mergeOffspring = function() {};
 
-        SPEA.prototype.calculateFitness = function() {};
+    SPEA.prototype.calculateFitness = function() {};
 
-        SPEA.prototype.environmentalSelection = function() {};
+    SPEA.prototype.environmentalSelection = function() {};
 
-        SPEA.prototype.matingSelection = function() {};
-    }
+    SPEA.prototype.matingSelection = function() {};
+
+    return SPEA;
 });
