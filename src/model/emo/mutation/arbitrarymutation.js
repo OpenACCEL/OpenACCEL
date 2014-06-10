1d553130286976d443f4a96785effe6784e952e3
Philippe Denissen <p.e.j.g.denissen@student.tue.nl>
2014-06-10 14:45:55 +0200

    Fix vector index bug and change new value into a double.

--- src/model/emo/mutation/arbitrarymutation.js
+++ src/model/emo/mutation/arbitrarymutation.js
@@ -38,14 +38,15 @@ define([], /**@lends Model.EMO.Mutation*/ function() {
          * @pre individual != null or undefined
          * @return {Individual}             the mutated individual
          */
-        ArbitraryMutation.prototype.mutate = function() {
+        ArbitraryMutation.prototype.mutate = function(individual) {
             Mutation.prototype.mutate.call(individual);
             var vector = individual.vector;
             // obtain the quantity to be mutated
-            var quantity = vector[Random.getRandomInt(0, vector.length - 1)];
+            var quantityIndex = Random.getRandomInt(0, vector.length - 1);
+            var quantity = vector[quantityIndex];
             // the new value for the quantity
-            var newValue = Random.getRandomInt(quantity.min, quantity.max);
-            individual.vector.value = newValue;
+            var newValue = Random.getRandomDouble(quantity.min, quantity.max);
+            vector[quantityIndex].value = newValue;
             return individual;
         };
 
