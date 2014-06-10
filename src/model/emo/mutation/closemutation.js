0ee4076343b41e545043e56e1a94babe6e78b92e
Jaczz <jacco_snoeren@live.nl>
2014-06-10 14:52:50 +0200

    Implementation closeMutation

--- src/model/emo/mutation/closemutation.js
+++ src/model/emo/mutation/closemutation.js
@@ -31,6 +31,12 @@ define([], /**@lends Model.EMO.Mutation*/ function() {
         CloseMutation.prototype = new Mutation();
 
         /**
+         * The percentage that determines the closeness of the new value
+         * @type {Number}
+         */
+        var PERCENTAGE = 0.05;
+
+        /**
          * Mutates one arbitrary category I quantity and gives it
          * a value close to the original value in the domain of that category.
          *
@@ -40,6 +46,19 @@ define([], /**@lends Model.EMO.Mutation*/ function() {
          */
         CloseMutation.prototype.mutate = function(individual) {
             Mutation.prototype.mutate.call(individual);
+            var vector = individual.vector;
+
+            // obtain the quantity to be mutated
+            var quantityIndex = Random.getRandomInt(0, vector.length - 1);
+            var quantity = vector[quantityIndex];
+
+            // obtain the new value for the quantity
+            var range = quantity.max - quantity.min;
+            var newMin = quantity.value - PERCENTAGE * range;
+            var newMax = quantity.value + PERCENTAGE * range;
+            var newValue = Random.getRandomDouble(newMin, newMax);
+            individual.vector[quantityIndex].value = newValue;
+            return individual;
         };
 
         return CloseMutation;
