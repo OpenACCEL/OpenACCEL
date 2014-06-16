be103c6b9ccc30adcb2f8a15630587372b2469a6
Shammah <contact@protoman.net>
2014-06-16 16:56:53 +0200

    Updated Script test cases.

--- test/model/script.js
+++ test/model/script.js
@@ -5,15 +5,21 @@ suite("Script", function() {
 
     setup(function(done) {
         requirejs(["assert", "model/script"], function(Assert, scriptModule) {
-            console.log("Loaded 'Script' module.");
             assert = Assert;
             Script = scriptModule;
             done();
         });
     });
 
-    suite("isComplete()", function() {
-        test("Script complete", function() {
+    suite("| Is Complete?", function() {
+        /**
+         * Simple script that should be complete.
+         *
+         * @input:      x = b
+         *              b = 4
+         * @expected:   isComplete() == true.
+         */
+        test("| Simple complete script", function() {
             var code = "x = b \n b=4";
             var script = new Script(code);
             var output = script.isComplete();
@@ -21,7 +27,13 @@ suite("Script", function() {
             assert.equal(output, expected);
         });
 
-        test("Undefined variable", function() {
+        /**
+         * An undefined variable should mean that the script is not yet complete.
+         * 
+         * @input:      x = b
+         * @expected:   isComplete() == false.
+         */
+        test("| Undefined variable", function() {
             var code = "x = b";
             var script = new Script(code);
             var output = script.isComplete();
@@ -29,14 +41,23 @@ suite("Script", function() {
             assert.equal(output, expected);
         });
 
-        test("Empty script", function() {
+        /**
+         * Empty scripts should not be complete by definition.
+         *
+         * @input: -
+         * @expected: isComplete() == false.
+         */
+        test("| Empty script", function() {
             var script = new Script();
             var output = script.isComplete();
             var expected = false;
             assert.equal(output, expected);
         });
 
-        test("Script empty after deleting quantities", function() {
+        /**
+         * When a script becomes empty after not being empty, is should not be complete.
+         */
+        test("| Script empty after deleting quantities", function() {
             var code = "x = b \n b=e+f\n e=3 \n f=6";
             var script = new Script(code);
 
