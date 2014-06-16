f37502db377568525d92c4e60482c74fbd57986a
Shammah <contact@protoman.net>
2014-06-16 16:22:05 +0200

    Updated ControllerAPI tests.

--- test/controller/ControllerAPI.js
+++ test/controller/ControllerAPI.js
@@ -5,16 +5,76 @@ suite("ControllerAPI", function() {
 
     setup(function (done) {
         requirejs(["assert", "controller/ControllerAPI"], function(Assert, ControllerAPI) {
-            console.log("Loaded 'Controller' module.");
             assert = Assert;
             controller = new ControllerAPI();
             done();
         });
     });
 
-    suite("Controlling", function() {
+    suite("| Execution", function() {
+        /**
+         * There should be an execution function.
+         *
+         * @input: -
+         * @expected: No exceptions.
+         */
+        test("| Normal execution", function() {
+            controller.execute;
+        });
 
-        test("Constructor tests", function() {
+        /**
+         * Pausing the controller should stop the controller from executing.
+         *
+         * @input: -
+         * @expected: Controller.executing == false.
+         */
+        test("| Pause execution", function() {
+            var expected = false;
+            controller.stop();
+            var output = controller.executing;
+            assert.equal(output, expected);
+        });
+    });
+
+    suite("| Quantities", function() {
+        /**
+         * There should be a getQuantities function.
+         *
+         * @input: -
+         * @expected: No exceptions.
+         */
+        test("| Get quantities", function() {
+            controller.getQuantities;
+        });
+
+        /**
+         * Adding a quantity should give no errors.
+         *
+         * @input: Two quantities: a = b + 10
+         *                         b = 10
+         * expected: 20.
+         */
+        test("| Add quantities", function() {
+            var input = "a = b + 10 ";
+            controller.addQuantity(input);
+            var input = "b = 10";
+            controller.addQuantity(input);
+            var output = controller.getQuantityValue('a');
+            var expected = 20;
+            assert.equal(output, expected);
+        });
+
+    });
+
+    suite("| Controller", function() {
+
+        /**
+         * Test the constructor.
+         *
+         * @input: -
+         * @expected: The controller should not be executing and the number of iterations should be zero.
+         */
+        test("| Constructor", function() {
             var expected = 0;
             var output = controller.numIterations;
             assert.equal(output,expected);
@@ -22,42 +82,18 @@ suite("ControllerAPI", function() {
             assert.equal(output, false);
         });
 
-        test("Normal Execution tests", function() {
-            controller.execute;
-        });
-
-        test("Pause Execution tests", function() {
-            var expected = false;
-            controller.stop();
-            var output = controller.executing;
-            assert.equal(output, expected);
-        });
-
-        test("Normal get quantities tests", function() {
-            controller.getQuantities;
-        });
-
-        test("Normal add quantity tests", function() {
-            controller.addQuantity("test = b");
-        });
-
-        test("Setting iterations to valid number", function() {
+        /**
+         * The controller should be able to have a limited number of executions.
+         * @input: 1
+         * @expected: 1
+         */
+        test("| Iterations", function() {
             var iterations = 1;
             var output = controller.numIterations;
             assert.equal(output, 0);
             controller.setIterations(iterations);
             output = controller.numIterations;
             var expected = 1;
-            assert.equal(output, expected);
-        });
-
-        test("Set value in model", function() {
-            var input = "a = b + 10 ";
-            controller.addQuantity(input);
-            var input = "b = 10";
-            controller.addQuantity(input);
-            var output = controller.getQuantityValue('a');
-            var expected = 20;
             assert.equal(output, expected);
         });
 
