ed09dc66091fedc43f3a58358a002f24b32cbb3e
FalFire <FalFire@users.noreply.github.com>
2014-06-17 11:45:56 +0200

    Fixed plot runtime error checking and throwing

--- src/model/library/plot.js
+++ src/model/library/plot.js
@@ -9,7 +9,7 @@ function plot(x) {
         "so there must be a single argument which is a vector. Each of the (1 or more) graphs has the form\n" +
         "graphi=[control,data,data,data,...] where 'control' is a javascript array assigning values\n" +
         "to parameters, and the 'data'-elements are vectors of scalar data each.";
-        return new Error(plotType);
+        throw new Error(plotTypeError);
     }
 
 }
