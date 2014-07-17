029af9db911925705be79166b54b83bd2dd266c0
Shammah <contact@protoman.net>
2014-07-17 11:59:00 +0200

    Remove unnecessary unit functions

--- src/model/library/units/paretoMax.js
+++
@@ -1,7 +0,0 @@
-function paretoMax(x) {
-    if (arguments.length != arguments.callee.length) {
-        throw new Error('Wrong number of arguments for ' + arguments.callee.name +
-            '. Expected: ' + arguments.callee.length + ', got: ' + arguments.length);
-    }
-    return x;
-}
