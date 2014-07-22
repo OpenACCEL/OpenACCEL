2fb80d7a40a93d0e13b638f6f5ce681b3d1b85e5
Shammah <contact@protoman.net>
2014-07-22 15:03:57 +0200

    Add atan2 unit test cases

--- test/model/library/atan2.js
+++ test/model/library/atan2.js
@@ -46,4 +46,30 @@ suite("Atan2 Library", function() {
         var output = compiler.compile(new script(input)).__z__();
         assert.equal(output, Math.atan2(3, Math.atan2(5, Math.atan2(5, 7) + 2)));
     });
+
+    suite("| Units", function() {
+        test("| Non-equal units", function() {
+            compiler.loadUnitsLib();
+            var input = 
+            "x = 5; kg\n" +
+            "y = 6; s\n" +
+            "z = atan2(x, y)";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+            assert.throws(output.__z__());
+        });
+
+        test("| Equal units", function() {
+            compiler.loadUnitsLib();
+            var input = 
+            "x = 5; kg\n" +
+            "y = 6; kg\n" +
+            "z = atan2(x, y)";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+            var z = output.__z__();
+            assert.equal(z.value, Math.atan2(5, 6));
+            assert.equal(true, z.equals(new UnitObject(0, {'kg': 1})));
+        });
+    });
 });
