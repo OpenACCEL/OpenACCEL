fb6cba2b73ba2d6b33933581f3cdc8774c315500
FalFire <FalFire@users.noreply.github.com>
2014-07-30 15:50:45 +0200

    2 more image function tests

--- test/Model/Library/iConvolve.js
+++ test/Model/Library/iConvolve.js
@@ -2,16 +2,19 @@ suite("iConvolve Library", function() {
     var compiler;
     var macros;
     var assert;
+    var script;
 
     setup(function(done) {
         // This saves the module for use in tests. You have to use
         // the done callback because this is asynchronous.
-        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader) {
+        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader, Script) {
             assert = Assert;
             compiler = new module();
             fileLoader = new FileLoader();
             fileLoader.load("iConvolve", "library");
             fileLoader.load("objecttoarray", "library");
+            fileLoader.load("UnitObject", "unitlibrary");
+            script = Script;
             done();
         });
     });
@@ -29,5 +32,37 @@ suite("iConvolve Library", function() {
         var result =iConvolve([[1,2,3],[1,2,3]],[[4,5,6],[4,5,6]],5,5,0);
 
         assert.deepEqual(result, expected);
+    });
+
+    suite("| Units", function() {
+        test("| Normal operation", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = iConvolve([[1,2,3],[1,2,3]],[[4,5,6],[4,5,6]],5,5,0)\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            assert.equal(true, UnitObject.prototype.isNormal(output.__a__()));
+            assert.deepEqual([[58,58,64],[58,58,64]], output.__a__().value);
+            assert.ifError(output.__a__().error);
+        });
+
+        test("| Error handling", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = [1,2,3] ; [kg,kg,kg]\n" +
+            "b = iConvolve([a,[1,2,3]],[[4,5,6],[4,5,6]],5,5,0)\n" +
+            "c = iConvolve([x,[1,2,3]],[[4,5,6],[4,5,6]],5,5,0)\n" +
+            "x = [1,2,3] ; d\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            assert.deepEqual([[58,58,64],[58,58,64]], output.__b__().value);
+            assert.equal(output.__b__().error, "unitError");
+
+            assert.deepEqual([[58,58,64],[58,58,64]], output.__c__().value);
+            assert.equal(output.__c__().error, "uncheckedUnit");
+            assert.ok(output.__c__().isNormal());
+        });
     });
 });
--- test/Model/Library/iSpike.js
+++ test/Model/Library/iSpike.js
@@ -2,16 +2,18 @@ suite("iSpike Library", function() {
     var compiler;
     var macros;
     var assert;
+    var script;
 
     setup(function(done) {
         // This saves the module for use in tests. You have to use
         // the done callback because this is asynchronous.
-        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader) {
+        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader, Script) {
             assert = Assert;
             compiler = new module();
             fileLoader = new FileLoader();
             fileLoader.load("iSpike", "library");
             fileLoader.load("objecttoarray", "library");
+            script = Script;
             done();
         });
     });
@@ -29,5 +31,39 @@ suite("iSpike Library", function() {
         var result =iSpike(0,0,3,3);
 
         assert.deepEqual(result, expected);
+    });
+
+    suite("| Units", function() {
+        test("| Normal operation", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = iSpike(0,0,3,3)\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            assert.equal(true, UnitObject.prototype.isNormal(output.__a__()));
+            assert.deepEqual([[1,0,0],[0,0,0],[0,0,0]], output.__a__().value);
+            assert.ifError(output.__a__().error);
+        });
+
+        test("| Error handling", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = 0 ; kg\n" +
+            "b = iSpike(a,0,3,3)\n" +
+            "c = iSpike(0,0,z,3)\n" +
+            "x = 3 ; d\n" +
+            "y = 3 ; kg\n" +
+            "z = and(x,y)\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            assert.deepEqual([[1,0,0],[0,0,0],[0,0,0]], output.__b__().value);
+            assert.equal(output.__b__().error, "unitError");
+
+            assert.deepEqual([[1,0,0],[0,0,0],[0,0,0]], output.__c__().value);
+            assert.equal(output.__c__().error, "uncheckedUnit");
+            assert.ok(output.__c__().isNormal());
+        });
     });
 });
