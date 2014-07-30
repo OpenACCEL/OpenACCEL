bf1804a3d0cf0332fb84df4bab9b4a6b0af66257
FalFire <FalFire@users.noreply.github.com>
2014-07-30 15:17:24 +0200

    2 more image function tests

--- test/Model/Library/iGaussian.js
+++ test/Model/Library/iGaussian.js
@@ -2,16 +2,19 @@ suite("iGaussian Library", function() {
     var compiler;
     var macros;
     var assert;
+    var script;
 
     setup(function(done) {
         // This saves the module for use in tests. You have to use
         // the done callback because this is asynchronous.
-        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader) {
+        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader, Script) {
             assert = Assert;
             compiler = new module();
             fileLoader = new FileLoader();
             fileLoader.load("iGaussian", "library");
+            fileLoader.load("and", "library");
             fileLoader.load("objecttoarray", "library");
+            script = Script;
             done();
         });
     });
@@ -29,5 +32,39 @@ suite("iGaussian Library", function() {
         var result = iGaussian(2,2,1,4);
 
         assert.deepEqual(result, expected);
+    });
+
+    suite("| Units", function() {
+        test("| Normal operation", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = iGaussian(2,2,1,4)\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            assert.equal(true, UnitObject.prototype.isNormal(output.__a__()));
+            assert.deepEqual([[0.25,0.25],[0.25,0.25]], output.__a__().value);
+            assert.ifError(output.__a__().error);
+        });
+
+        test("| Error handling", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = 2 ; kg\n" +
+            "b = iGaussian(a,2,1,4)\n" +
+            "c = iGaussian(z,2,1,4)\n" +
+            "x = 2 ; d\n" +
+            "y = 2 ; kg\n" +
+            "z = and(x,y)\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            assert.deepEqual([[0.25,0.25],[0.25,0.25]], output.__b__().value);
+            assert.equal(output.__b__().error, "unitError");
+
+            assert.deepEqual([[0.25,0.25],[0.25,0.25]], output.__c__().value);
+            assert.equal(output.__c__().error, "uncheckedUnit");
+            assert.ok(output.__c__().isNormal());
+        });
     });
 });
--- test/Model/Library/iMake.js
+++ test/Model/Library/iMake.js
@@ -2,16 +2,23 @@ suite("iMake Library", function() {
     var compiler;
     var macros;
     var assert;
+    var script;
 
     setup(function(done) {
         // This saves the module for use in tests. You have to use
         // the done callback because this is asynchronous.
-        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader) {
+        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader, Script) {
             assert = Assert;
             compiler = new module();
             fileLoader = new FileLoader();
             fileLoader.load("iMake", "library");
             fileLoader.load("objecttoarray", "library");
+            fileLoader.load("UnitObject", "unitlibrary");
+            fileLoader.load("unaryZip", "library");
+            fileLoader.load("binaryZip", "library");
+            fileLoader.load("multiaryZip", "library");
+            fileLoader.load("zip", "library")
+;            script = Script;
             done();
         });
     });
@@ -29,5 +36,42 @@ suite("iMake Library", function() {
         var result =iMake(3,2,2);
 
         assert.deepEqual(result, expected);
+    });
+
+    suite("| Units", function() {
+        test("| Normal operation", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = iMake(7,2,3)\n" +
+            "b = 25 ; kg\n" +
+            "c = iMake(b,2,2)\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            assert.equal(true, UnitObject.prototype.isNormal(output.__a__()));
+            assert.deepEqual(UnitObject.prototype.create([[7,7,7],[7,7,7]]), output.__a__());
+            assert.ifError(output.__a__().error);
+
+            assert.deepEqual(UnitObject.prototype.create([[25,25],[25,25]], {'kg':1}), output.__c__());
+            assert.ifError(output.__c__().error);
+        });
+
+        test("| Error handling", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = false ; d\n" +
+            "b = 40 ; kg\n" +
+            "c = and(a,b)\n" +
+            "z = and(c, true)\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            assert.equal(false, output.__c__().value);
+            assert.equal(output.__c__().error, "unitError");
+
+            assert.equal(false, output.__z__().value);
+            assert.equal(output.__z__().error, "uncheckedUnit");
+            assert.ok(output.__z__().isNormal());
+        });
     });
 });
