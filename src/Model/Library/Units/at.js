9963a31972db6f3a81aa59ec863cd35311a8a5f6
Shammah <contact@protoman.net>
2014-08-01 12:05:25 +0200

    Add vector support for at

--- src/Model/Library/Units/at.js
+++ src/Model/Library/Units/at.js
@@ -3,27 +3,12 @@ function at(x, y) {
         throw new Error('Wrong number of arguments for ' + '@' +
             '. Expected: ' + arguments.callee.length + ', got: ' + arguments.length);
     }
-    if (y instanceof Array) {
-        // Recursive step, y is an array
-        var result = [];
-        for (var key in y) {
-            result[key] = at(x, y[key]);
-        }
-        return result;
-    } else {
-        if (!isNaN(y)) {
-            y = Math.round(y);
-        }
-        // Base: y is a scalar
-        if (x instanceof Array) {
-            if (x[y] === undefined) {
-                return [];
-            } else {
-                return x[y];
-            }
+
+    return exe.lib.std.at(x, unaryZip(y, function(a) {
+        if (a instanceof UnitObject) {
+            return a.value;
         } else {
-            //If x is scalar we simply use x instead of x[y]
-            return x;
+            return a;
         }
-    }
+    }));
 }
