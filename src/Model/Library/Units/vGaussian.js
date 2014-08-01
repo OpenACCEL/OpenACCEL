a546ae6be548e61b2860569d26245f418746fde6
Shammah <contact@protoman.net>
2014-08-01 09:15:51 +0200

    Add unit support for vGaussian

--- src/Model/Library/Units/vGaussian.js
+++ src/Model/Library/Units/vGaussian.js
@@ -4,27 +4,29 @@ function vGaussian(x, y) {
         throw new Error('Wrong number of arguments for ' + arguments.callee.name +
             '. Expected: ' + arguments.callee.length + ', got: ' + arguments.length);
     }
-    if (!(x instanceof Array) && !(y instanceof Array)) {
-        var n = Math.round(x);
-        var s = y;
-        if (n >= 0) {
-            var t = [];
-            var sum = 0;
-            var x = -(n - 1) / 2;
-            var denom = 2 * s * s;
-            for (i = 0; i < n; i++) {
-                t[i] = Math.exp(-x * x / denom);
-                sum += t[i];
-                x += 1;
-            }
-            for (i = 0; i < n; i++) {
-                t[i] /= sum;
-            }
-            return t;
-        } else {
-            throw new Error("vGaussian: cannot make a vector with <0 elements");
-        }
-    } else {
-        throw new Error("vGaussian: both arguments must be scalar.");
+
+    if (!(x instanceof UnitObject)) {
+        x = new UnitObject(x);
     }
+
+    if (!(y instanceof UnitObject)) {
+        y = new UnitObject(y);
+    }
+
+    var std_vGaussian = exe.lib.std.vGaussian;
+    var error = UnitObject.prototype.propagateError(std_vGaussian, x, y);
+    if (error) {
+        return error;
+    }
+
+    var ans = std_vGaussian(x.value, y.value);
+    if (x.hasUnit() || y.hasUnit()) {
+        return unaryZip(ans, function(a) {
+            return new UnitObject(a, {}, "vGaussian arguments must be unitless.");
+        });
+    }
+
+    return unaryZip(ans, function(a) {
+        return new UnitObject(a);
+    });
 }
--- test/Model/Library/vGaussian.js
+++ test/Model/Library/vGaussian.js
@@ -2,21 +2,23 @@ suite("vGaussian Library", function() {
     var compiler;
     var macros;
     var assert;
+    var script;
 
     setup(function(done) {
         // This saves the module for use in tests. You have to use
         // the done callback because this is asynchronous.
-        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader) {
+        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader, Script) {
             assert = Assert;
             compiler = new module();
             fileLoader = new FileLoader();
             fileLoader.load("vGaussian", "library");
             fileLoader.load("objecttoarray", "library");
+            script = Script;
             done();
         });
     });
 
-    suite("vGaussian", function() {
+    suite("| vGaussian", function() {
 
         /**
          * Test case for vGuassian.
@@ -25,7 +27,7 @@ suite("vGaussian Library", function() {
          * @input: vGaussian(6,3)
          * @expected: [0.14,0.17,0.19,0.19,0.17,0.14]
          */
-        test("Example from help vGaussian(6,3)=[0.14,0.17,0.19,0.19,0.17,0.14]", function() {
+        test("| Example from help vGaussian(6,3)=[0.14,0.17,0.19,0.19,0.17,0.14]", function() {
             eval(fileLoader.getContent());
             var expected = [0.14, 0.17, 0.19, 0.19, 0.17, 0.14];
             var result = vGaussian(6, 3);
@@ -53,7 +55,7 @@ suite("vGaussian Library", function() {
          * @input: vGaussian(10,3)
          * @expected: [0.048, 0.074, 0.10, 0.13, 0.14, 0.14, 0.13, 0.10, 0.074, 0.048]
          */
-        test("Example from help vGaussian(8,3)=[0.048,0.074,0.10,0.13,0.14,0.14,0.13,0.10,0.074,0.048]", function() {
+        test("| Example from help vGaussian(8,3)=[0.048,0.074,0.10,0.13,0.14,0.14,0.13,0.10,0.074,0.048]", function() {
             eval(fileLoader.getContent());
 
             var expected = [0.048, 0.074, 0.10, 0.13, 0.14, 0.14, 0.13, 0.10, 0.074, 0.048];
@@ -73,5 +75,41 @@ suite("vGaussian Library", function() {
             assert.deepEqual(result, expected);
         });
 
+    });
+
+    suite("| Units", function() {
+        test("| Argument should be unitless", function() {
+            compiler.loadUnitsLib();
+            var input = 
+            "a = 6\n" +
+            "b = 3\n" +
+            "c = 6; kg\n" +
+            "d = 3; kg\n" +
+            "w = vGaussian(a, b)\n" +
+            "x = vGaussian(a, d)\n" +
+            "y = vGaussian(c, b)\n" +
+            "z = vGaussian(c, d)\n";
+            var expected = [0.14,0.17,0.19,0.19,0.17,0.14];
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            // round all the results to two decimals
+
+            for (var i = 0; i < 5; i++) {
+                assert.ifError(output.__w__()[i].error);
+                assert.ok(output.__x__()[i].error);
+                assert.ok(output.__y__()[i].error);
+                assert.ok(output.__z__()[i].error);
+
+                assert.equal(true, output.__w__()[i].isNormal());
+                assert.equal(true, output.__x__()[i].isNormal());
+                assert.equal(true, output.__y__()[i].isNormal());
+                assert.equal(true, output.__z__()[i].isNormal());
+                assert.equal(expected[i], Math.round(output.__x__()[i].value * 100) / 100);
+                assert.equal(expected[i], Math.round(output.__y__()[i].value * 100) / 100);
+                assert.equal(expected[i], Math.round(output.__z__()[i].value * 100) / 100);
+                assert.equal(expected[i], Math.round(output.__w__()[i].value * 100) / 100);
+            }
+        });
     });
 });
