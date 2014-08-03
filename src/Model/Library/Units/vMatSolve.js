56d19d8353d730e94086198a51e53be43017c0b7
FalFire <FalFire@users.noreply.github.com>
2014-08-03 14:54:35 +0200

    vMatSolve unit support

--- src/Model/Library/Units/vMatSolve.js
+++ src/Model/Library/Units/vMatSolve.js
@@ -3,63 +3,35 @@ function vMatSolve(mm, v) {
         throw new Error('Wrong number of arguments for ' + arguments.callee.name +
             '. Expected: ' + arguments.callee.length + ', got: ' + arguments.length);
     }
-    var isOK = true;
-    if (mm instanceof Array) {
-        var t = [];
-        var u = [];
-        // n is number of rows; m is number of columns of the matrix (=mm)
-        var n = mm.length;
-        if (mm[0] instanceof Array) {
-            var m = mm[0].length;
-            if (v instanceof Array) {
-                if (m <= n && v.length == n) {
-                    for (i = 0; i < n; i++) {
-                        t[i] = [];
-                        if (mm[i] instanceof Array) {
-                            if (mm[i].length == m) {
-                                for (j = 0; j < m; j++) {
-                                    if (!(mm[i][j] instanceof Array)) {
-                                        t[i][j] = mm[i][j];
-                                    } else {
-                                        throw new Error("\nvMatSolve: every matrix element must be scalar");
-                                    }
-                                }
-                            } else {
-                                throw new Error("\nvMatSolve: every row in left hand side must be of equal length");
-                            }
-                        } else {
-                            throw new Error("\nvMatSolve: every row in left hand side must be a vector");
-                        }
-                    }
-                    // next assemble the right hand vector
-                    for (i = 0; i < n; i++) {
-                        if (!(v[i] instanceof Array)) {
-                            u[i] = [];
-                            u[i][0] = v[i];
-                        } else {
-                            throw new Error("\nvMatSolve: non-scalar element found in right-hand side");
-                        }
-                    }
-                } else {
-                    throw new Error("\nvMatSolve: nr. rows of right hand side must be equal to nr. columns of left hand side, and the number of rows of the matrix must not be smaller than the number of columns");
-                }
-            } else {
-                throw new Error("\nvMatSolve: right hand side must be vector");
-            }
+
+    // Convert all to UnitObjects
+    m = unaryZip(mm, function(a) {
+        if (!(a instanceof UnitObject)) {
+            return new UnitObject(a);
         } else {
-            throw new Error("\nvMatSolve: left hand side must be vector of vectors");
+            return a;
         }
-    } else {
-        throw new Error("\nvMatSolve: first argument must be vector");
-    }
-    if (isOK) {
-        var aA = Matrix.create(t);
-        var aB = Matrix.create(u);
-        var aSol = Matrix.solve(aA, aB);
-        var tt = [];
-        for (i = 0; i < aSol.mat.length; i++) {
-            tt[i] = aSol.mat[i][0];
+    });
+    v = unaryZip(v, function(a) {
+        if (!(a instanceof UnitObject)) {
+            return new UnitObject(a);
+        } else {
+            return a;
         }
-        return tt
+    });
+
+    var std_vms = exe.lib.std.vMatSolve;
+    var error = UnitObject.prototype.propagateError(std_vms, m, v);
+    if (error) {
+        return error;
     }
+
+    var mValues = UnitObject.prototype.toArray(m);
+    var vValues = UnitObject.prototype.toArray(v);
+    var mUnit = m[0][0];
+    var vUnit = v[0];
+
+    var ansUnit = vUnit.divide(mUnit).unit;
+    var ans = UnitObject.prototype.create(std_vms(mValues, vValues), ansUnit);
+    return ans;
 }
--- test/Model/Library/vMatSolve.js
+++ test/Model/Library/vMatSolve.js
@@ -2,15 +2,17 @@ suite("vMatSolve Library", function() {
     var compiler;
     var macros;
     var assert;
+    var script;
 
     setup(function(done) {
         // This saves the module for use in tests. You have to use
         // the done callback because this is asynchronous.
-        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader) {
+        requirejs(["assert", "Model/Compiler", "Model/FileLoader", "Model/Script"], function(Assert, module, FileLoader, Script) {
             assert = Assert;
             compiler = new module();
             fileLoader = new FileLoader();
             fileLoader.load("Functions", "library");
+            script = Script;
             done();
         });
     });
@@ -35,5 +37,21 @@ suite("vMatSolve Library", function() {
             assert.deepEqual(result, expected);
         });
 
+    });
+
+    suite("| Units", function() {
+        test("| Normal operation", function() {
+            compiler.loadUnitsLib();
+            var input =
+            "a = [[1,2],[3,4]] ; [[kg, kg], [kg, kg]]\n" +
+            "b = [1,2] ; [kg2, kg2]\n" +
+            "z = vMatSolve(a,b)\n";
+            var output = compiler.compile(new script(input));
+            output.setUnits(true);
+
+            var expected = UnitObject.prototype.create([0, 0.50], {'kg':1});
+            assert.deepEqual(output.__z__(), expected);
+            assert.ifError(output.__z__().error);
+        });
     });
 });
