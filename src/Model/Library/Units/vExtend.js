050a01c5f7c4b4f0f1026b2ada3a66846fecf929
Shammah <contact@protoman.net>
2014-08-06 15:09:21 +0200

    Add unit support for vExtend

--- src/Model/Library/Units/vExtend.js
+++ src/Model/Library/Units/vExtend.js
@@ -3,39 +3,8 @@ function vExtend(x, y) {
         throw new Error('Wrong number of arguments for ' + arguments.callee.name +
             '. Expected: ' + arguments.callee.length + ', got: ' + arguments.length);
     }
-    if (!(x instanceof Array)) {
-        if (y instanceof Array) {
-            var p = [];
-            p.push(x);
-            for (k in y) {
-                if (!isNaN(k)) {
-                    p.push(y[k]);
-                } else {
-                    p[k] = y[k];
-                }
-            }
-            return p;
-        } else {
-            return [x, y];
-        }
-    } else {
-        var p = [];
-        for (k in x) {
-            p[k] = x[k];
-        }
-        if (y instanceof Array) {
-            for (k in y) {
-                if (!isNaN(k)) {
-                    p.push(y[k]);
-                } else {
-                    p[k] = y[k];
-                }
-            }
-        } else {
-            p.push(y);
-        }
-        return p;
-    }
+
+    return exe.lib.std.vExtend(x, y);
 }
 
 vExtend.base = []
