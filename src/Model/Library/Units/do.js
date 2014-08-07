806675126c3edc4fe8d00330b21eaab164ccafa9
Shammah <contact@protoman.net>
2014-08-07 14:26:00 +0200

    Fix crash for unit function of do. No implementation yet.

--- src/Model/Library/Units/do.js
+++ src/Model/Library/Units/do.js
@@ -3,24 +3,14 @@ function __do__(code, args) {
         throw new Error('Wrong number of arguments for ' + arguments.callee.name +
             '. Expected: ' + arguments.callee.length + ', got: ' + arguments.length);
     }
-    if (typeof code === "string") {
-        if (args instanceof Object) {
-            for (var arg in args) {
-                var target = new RegExp('_' + arg, "g");
-                code = code.replace(target, JSON.stringify(args[arg]));
-                try {
-                    // this is to protect against all disasters like syntax errors in the script string code we can't foresee
-                    var res = (new Function(code))();
-                    return res;
-                } catch (err) {
-                    return 'ERROR';
-                }
-            }
-        } else {
-            throw new Error("\nFor function do(), second argument must be a vector");
-  
-        }
-    } else {
-        throw new Error("\nFor function do(), first argument must be a string (= a code fragment)");
-    }
+
+    // Translate arguments from UnitObject, invoke normal function and translate back to a normal unit.
+    var code = code.value;
+    var args = unaryZip(args, function(x) {
+        return x.value;
+    });
+
+    return unaryZip(exe.lib.std.__do__(code, args), function(x) {
+        return new UnitObject(x);
+    });
 }
